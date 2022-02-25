9533d67a91e4cd481f3802e805343c2689f5261e
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-02-25 20:52:57 +0100

    Set up cerberus validation of config files

--- packages/core/src/main.py
+++ packages/core/src/main.py
@@ -4,6 +4,10 @@ from src.validation import Validation
 
 
 def run():
+    Validation.check_parameters_config()
+    Validation.check_setup_config()
+
+    """
     while True:
         # TODO: Validate setup and parameters
 
@@ -11,8 +15,4 @@ def run():
 
         # TODO: Sleep time per iteration specified in parameters
 
-        pass
-
-
-if __name__ == "__main__":
-    run()
+        pass"""
--- packages/core/src/validation.py
+++ packages/core/src/validation.py
@@ -1,10 +1,21 @@
+import json
 import logging
 import os
+import cerberus
 
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(dir(os.path.abspath(__file__)))))
 SETUP_FILE_PATH = f"{PROJECT_DIR}/config/setup.json"
 PARAMS_FILE_PATH = f"{PROJECT_DIR}/config/parameters.json"
+
+SETUP_FILE_VALIDATOR = cerberus.Validator({})
+PARAMS_FILE_VALIDATOR = cerberus.Validator(
+    {"secondsPerIteration": {"type": "number", "min": 0}}
+)
+
+
+class CerberusException(Exception):
+    pass
 
 
 class Validation:
@@ -12,12 +23,36 @@ class Validation:
     def check_setup_config():
         try:
             assert os.path.isfile(SETUP_FILE_PATH), "file does not exist"
+            with open(SETUP_FILE_PATH, "r") as f:
+                try:
+                    SETUP_FILE_CONTENT = json.load(f)
+                except:
+                    raise AssertionError("not in a valid json format")
+
+            if not SETUP_FILE_VALIDATOR.validate(SETUP_FILE_CONTENT):
+                raise CerberusException(SETUP_FILE_VALIDATOR.errors)
+            # TODO: Remaining checks that cannot be done with cerberus
+
         except AssertionError as a:
             logging.error(f"Error in setup file: {a}")
+        except CerberusException as v:
+            logging.error(f"Error in parameters file: {v}")
 
     @staticmethod
     def check_parameters_config():
         try:
             assert os.path.isfile(PARAMS_FILE_PATH), "file does not exist"
+            with open(PARAMS_FILE_PATH, "r") as f:
+                try:
+                    PARAMS_FILE_CONTENT = json.load(f)
+                except:
+                    raise AssertionError("not in a valid json format")
+
+            if not PARAMS_FILE_VALIDATOR.validate(PARAMS_FILE_CONTENT):
+                raise CerberusException(PARAMS_FILE_VALIDATOR.errors)
+            # TODO: Remaining checks that cannot be done with cerberus
+
         except AssertionError as a:
             logging.error(f"Error in parameters file: {a}")
+        except CerberusException as v:
+            logging.error(f"Error in parameters file: {v}")
