6ae67ec97c0e9627e0cabaaf47a727e5457cac2f
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-03-02 14:59:26 +0100

    Implement lock-file for config JSONs

--- packages/core/main.py
+++ packages/core/main.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import time
+from filelock import FileLock
 
 from packages.core.opus_measurement import OpusMeasurement
 from packages.core.sun_tracking import SunTracking
@@ -13,21 +14,18 @@ dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
 SETUP_FILE_PATH = f"{PROJECT_DIR}/config/setup.json"
 PARAMS_FILE_PATH = f"{PROJECT_DIR}/config/parameters.json"
+CONFIG_LOCK_PATH = f"{PROJECT_DIR}/config/config.lock"
 
 # Setup logging module
 logging_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(
     level=logging.DEBUG, filename="logs/debug.log", format=logging_format
 )
-
 logging_info_handler = logging.FileHandler(filename="logs/info.log", mode="a")
 logging_info_handler.setLevel(logging.INFO)
 logging_info_handler.setFormatter(logging.Formatter(logging_format))
-
 logger = logging.getLogger("pyra.core")
 logger.addHandler(logging_info_handler)
-
-# https://docs.python.org/3/howto/logging.html#handlers
 
 
 def run():
@@ -35,13 +33,16 @@ def run():
         execution_started_at = datetime.now().timestamp()
         logger.info("Starting Iteration")
 
-        # TODO: lock config and parameter files during read operation
-        Validation.check_parameters_config()
-        Validation.check_setup_config()
-        with open(SETUP_FILE_PATH, "r") as f:
-            SETUP = json.load(f)
-        with open(PARAMS_FILE_PATH, "r") as f:
-            PARAMS = json.load(f)
+        # FileLock = Mark, that the config JSONs are being used and the
+        # CLI should not interfere. A file "config/config.lock" will be created
+        # and the existence of this file will make the next line wait.
+        with FileLock(CONFIG_LOCK_PATH):
+            Validation.check_parameters_config()
+            Validation.check_setup_config()
+            with open(SETUP_FILE_PATH, "r") as f:
+                SETUP = json.load(f)
+            with open(PARAMS_FILE_PATH, "r") as f:
+                PARAMS = json.load(f)
 
         # TODO: Possibly handle communication between these modules
         # TODO: Pass SETUP and PARAMS to modules
