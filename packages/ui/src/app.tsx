900db22ff466e8e5fb777864149a571f255b1473
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-03-13 18:25:58 +0100

    pyra-ui - Improve header design

--- packages/ui/src/app.tsx
+++ packages/ui/src/app.tsx
@@ -1,44 +1,38 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const tabs = [
-    { label: 'First Tab', id: 'first' },
-    { label: 'Second Tab', id: 'second' },
-    { label: 'Third Tab', id: 'third' },
-    { label: 'Fourth Tab', id: 'fourth' },
-    { label: 'Fifth Tab', id: 'fifth' },
-];
+const tabs = ['Status', 'Setup', 'Parameters', 'Logs', 'Enclosure Controls'];
 
 function Main() {
-    const [activeTab, setActiveTab] = React.useState(tabs[0].id);
+    const [activeTabIndex, setActiveTabIndex] = React.useState(0);
 
     return (
         <div className='flex flex-col items-stretch w-screen h-screen'>
-            <header className='w-full flex-col px-6 pt-4'>
-                <h1 className='text-2xl font-bold pb-2'>
-                    Pyra{' '}
-                    <span className='font-light bg-amber-200 text-amber-700 text-sm rounded px-1 py-0.5 ml-1'>
+            <header className='flex-col w-full pt-4 bg-slate-900'>
+                <h1 className='pb-1 text-3xl font-bold text-center text-white'>
+                    PyRa{' '}
+                    <span className='font-normal bg-amber-200 text-amber-700 text-sm rounded px-1 py-[0.0625rem] ml-1.5'>
                         v4.0.0
                     </span>
                 </h1>
-                <div className='w-full flex flex-wrap py-2 gap-x-1 gap-y-1 border-b'>
-                    {tabs.map(t => (
+                <div className='flex flex-wrap justify-center w-full px-4 py-3 gap-x-2 gap-y-2'>
+                    {tabs.map((t, i) => (
                         <button
                             className={
-                                'px-3 py-0.5 rounded font-semibold cursor-pointer ' +
-                                (t.id === activeTab
-                                    ? 'bg-slate-300 text-black '
-                                    : 'bg-white text-slate-500 hover:bg-slate-100 hover:text-slate-600')
+                                'px-3 py-0.5 rounded font-medium cursor-pointer ' +
+                                (i === activeTabIndex
+                                    ? 'bg-slate-600 text-white '
+                                    : 'bg-slate-800 text-slate-400 hover:bg-slate-700 hover:text-slate-200')
                             }
-                            onClick={() => setActiveTab(t.id)}
+                            onClick={() => setActiveTabIndex(i)}
                         >
-                            {t.label}
+                            {t}
                         </button>
                     ))}
                 </div>
             </header>
             <main className='flex items-start justify-center flex-grow p-6'>
-                <div className=' rounded shadow bg-gray-900 text-gray-100 px-2 py-1'>
+                <div className='px-2 py-1 text-gray-100 bg-gray-900 rounded shadow '>
                     React + Tailwind + Typescript + Electron = ❤
                 </div>
             </main>
