4a889af7ca9a1767cdc961a7173530e55440ed00
patrickjaigner <pataigner.login@gmail.com>
2022-03-15 17:42:14 +0100

    Base suntracker structure

--- packages/core/sun_tracking.py
+++ packages/core/sun_tracking.py
@@ -10,6 +10,7 @@
 
 
 import logging
+import pywin32
 
 logger = logging.getLogger("pyra.core")
 
@@ -18,4 +19,37 @@ class SunTracking:
     @staticmethod
     def run():
         logger.info("Running SunTracking")
+        pass
+
+    def __start_sun_tracking_automation(self):
+        #delete stop.txt file in camtracker folder if present
+        #exe call with -automation
+        camtracker_call = "c/:camtracker.exe -automation"
+        hProcess, hThread, dwProcessId, dwThreadId = pywin32.CreateProcess(None, camtracker_call, None, None, 0,
+                                                                           win32con.NORMAL_PRIORITY_CLASS, None,
+                                                                           None, None)
+
+
+    def __stop_sun_tracking_automation(self):
+        #create stop.txt file in camtracker folder
+        pass
+
+    def __get_vbdsd_sun_status(self):
+        #reads for outside sun conditions to start tracking
+        #return
+        pass
+
+    def __get_camtracker_sun_status(self):
+        #check sun status logged by camtracker
+        pass
+
+    def __get_camtracker_position(self):
+        pass
+
+    def __move_enclosure_cover(self):
+        #move enclosure cover to sun positin
+        pass
+
+    def __read_motor_offsets(self):
+        #read azimuth and elevation motor offsets from camtracker logfiles
         pass
