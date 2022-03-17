21d380626c1c4b9c893e19005abf2181de3f5992
patrickjaigner <pataigner.login@gmail.com>
2022-03-17 14:02:36 +0100

    formatting, pep8

--- packages/core/sun_tracking.py
+++ packages/core/sun_tracking.py
@@ -19,38 +19,83 @@ logger = logging.getLogger("pyra.core")
 class SunTracking:
 
     def __init__(self):
-        # note: dde servers talk to dde servers
-        self.camtracker_process = (None, None, None, None)
         self._PARAMS = {}
         self._SETUP = {}
+        self.camtracker_process = (None, None, None, None)
+        self.camtracker_application_status = 0
 
 
     def run(self, setup: dict, params: dict):
         logger.info("Running SunTracking")
         self.__update_json_config(setup, params)
+
+        #check for PYRA Test Mode status
+        if self._PARAMS["PYRA_test_mode"] == 1:
+            return
+
+
+        #startup camtrackerr automation: startup conditions given and inactive
+        #stop camtracker automation: stop conditions given and active
+
         pass
 
     def __update_json_config(self, setup: dict, params: dict):
+        """Updates class internal dictionaries to the latest version of the
+        json files for SETUP and PARAMETERS.
+        """
         self._SETUP = setup
         self._PARAMS = params
 
+    def __update_camtracker_application_status(self):
+        """Updates the parameter self.camtracker_application_status.
+        Uses win32process from pywin32 module to check hProcess available
+        in self.camtracker_process.
+
+        0 if Application is currently not running on OS
+        1 if Application is currently running on OS
+        """
+        # TODO: implement functionality
+        pass
+
     def __start_sun_tracking_automation(self):
+        """Uses win32process frm pywin32 module to start up the CamTracker
+         executable with additional paramter -automation.
+        The paramter - automation will instruct CamTracker to automatically
+        move the mirrors to the expected sun position during startup.
+
+         Returns pywin32 process information for later usage."""
         #delete stop.txt file in camtracker folder if present
         #exe call with -automation
         # http://timgolden.me.uk/pywin32-docs/win32process.html
-        camtracker_call = self._PARAMS["CamTracker_executable_full_path"] + " -automation"
-        hProcess, hThread, dwProcessId, dwThreadId = pywin32.CreateProcess(None, camtracker_call, None, None, 0,
-                                                                           win32con.NORMAL_PRIORITY_CLASS, None,
-                                                                           None, None)
+        camtracker_call = self._PARAMS["CamTracker_executable_full_path"] \
+                          + " -automation"
+        hProcess, hThread, dwProcessId, dwThreadId = pywin32.CreateProcess(
+            None,
+            camtracker_call,
+            None,
+            None,
+            0,
+            win32con.NORMAL_PRIORITY_CLASS,
+            None,
+            None,
+            None)
 
         return (hProcess, hThread, dwProcessId, dwThreadId)
 
     def __stop_sun_tracking_automation(self):
+        """Tells the CamTracker application to end program and move mirrors
+        to parking position.
+
+        CamTracker has an internal check for a stop.txt file in its directory
+        and will do a clean shutdown.
+        """
+
         #create stop.txt file in camtracker folder
-        camtracker_directory = os.path.dirname(self._PARAMS["CamTracker_executable_full_path"])
+        camtracker_directory = os.path.dirname(
+            self._PARAMS["CamTracker_executable_full_path"])
         f = open(camtracker_directory + "stop.txt", 'w')
         f.close()
-        pass
+
 
     def __get_vbdsd_sun_status(self):
         #reads for outside sun conditions to start tracking
