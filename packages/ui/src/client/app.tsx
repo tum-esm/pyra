d77e6e6ef9eb5f99cd705df1adb40b6d5aece32a
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-04-05 16:38:57 +0200

    pyra-ui - Implement pyra-cli check in client

--- packages/ui/src/client/app.tsx
+++ packages/ui/src/client/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import LogTab from './tabs/log-tab';
 import Header from './components/header';
@@ -7,24 +7,62 @@ const tabs = ['Status', 'Setup', 'Parameters', 'Logs', 'Enclosure Controls'];
 
 function Main() {
     const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [pyraIsSetUp, setPyraIsSetUp] = useState(false);
+    const [checkingSetup, setCheckingSetup] = useState(true);
 
-    // TODO: Check for pyra-cli state on startup
-    // TODO: Render placeholder tab with instructions
-    // TODO: Add button to refresh pyra-cli state
+    async function updateCliStatus() {
+        setCheckingSetup(true);
+        const status = await window.electron.checkCliStatus();
+        setPyraIsSetUp(status);
+        setCheckingSetup(false);
+    }
+
+    useEffect(() => {
+        if (!pyraIsSetUp) {
+            updateCliStatus();
+        }
+    }, []);
 
     return (
         <div className='flex flex-col items-stretch w-screen h-screen overflow-hidden'>
-            <Header {...{ tabs, activeTabIndex, setActiveTabIndex }} />
-            <main className='flex-grow w-full min-h-0 bg-slate-200'>
-                <div
-                    className={
-                        'w-full h-full ' +
-                        (tabs[activeTabIndex] === 'Logs' ? 'block ' : 'hidden')
-                    }
-                >
-                    <LogTab />
-                </div>
-            </main>
+            {!pyraIsSetUp && !checkingSetup && (
+                <main className='flex flex-col items-center justify-center w-full h-full gap-y-4'>
+                    <p className='max-w-sm text-center'>
+                        <pre className='bg-slate-200 mr-1 px-1 py-0.5 rounded-sm font-bold inline'>
+                            pyra-cli
+                        </pre>{' '}
+                        has not been found on your system. Please following the
+                        installation instructions on{' '}
+                        <span className='font-bold text-blue-500 underline'>
+                            https://github.com/tum-esm/pyra
+                        </span>
+                        .
+                    </p>
+                    <button
+                        onClick={updateCliStatus}
+                        className='px-2 py-0.5 text-green-800 bg-green-200 rounded font-medium hover:bg-green-300 hover:text-green-900'
+                    >
+                        Retry connection
+                    </button>
+                </main>
+            )}
+            {pyraIsSetUp && (
+                <>
+                    <Header {...{ tabs, activeTabIndex, setActiveTabIndex }} />
+                    <main className='flex-grow w-full min-h-0 bg-slate-200'>
+                        <div
+                            className={
+                                'w-full h-full ' +
+                                (tabs[activeTabIndex] === 'Logs'
+                                    ? 'block '
+                                    : 'hidden')
+                            }
+                        >
+                            <LogTab />
+                        </div>
+                    </main>
+                </>
+            )}
         </div>
     );
 }
