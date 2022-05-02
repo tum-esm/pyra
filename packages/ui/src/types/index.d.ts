ba0c797a7802bc36281c34cfcc6af80152c0064e
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-02 15:39:02 +0200

    pyra-ui - Update config schemas

--- packages/ui/src/types/index.d.ts
+++ packages/ui/src/types/index.d.ts
@@ -12,31 +12,116 @@ declare global {
     }
 }
 
+type int_array_3 = [number, number, number];
+type int_array_4 = [number, number, number, number];
+
 namespace TYPES {
     export type setupJSON = {
-        enclosure: {
-            tum_enclosure_is_present: boolean;
+        camtracker: {
+            config_path: string;
+            executable_path: string;
+            learn_az_elev_path: string;
+            sun_intensity_path: string;
         };
         em27: {
             ip: string;
         };
-        plc: {
-            is_present: boolean;
-            ip: string;
-        };
-        camtracker: {
-            executable_path: string;
-            learn_az_elev_path: string;
-            sun_intensity_path: string;
-            config_path: string;
+        enclosure: {
+            tum_enclosure_is_present: boolean;
         };
         opus: {
             executable_path: string;
         };
+        plc: {
+            actors: {
+                cover_closed: int_array_4;
+                current_angle: int_array_4;
+                fan_speed: int_array_4;
+                move_cover: int_array_4;
+                nominal_angle: int_array_4;
+            };
+            control: {
+                auto_temp_mode: int_array_4;
+                manual_control: int_array_4;
+                manual_temp_mode: int_array_4;
+                reset: int_array_4;
+                sync_to_tracker: int_array_4;
+            };
+            ip: string;
+            is_present: boolean;
+            power: {
+                camera: int_array_4;
+                computer: int_array_4;
+                heater: int_array_4;
+                router: int_array_4;
+                spectrometer: int_array_4;
+            };
+            sensors: {
+                humidity: int_array_4;
+                temperature: int_array_4;
+            };
+            state: {
+                camera: int_array_4;
+                computer: int_array_4;
+                cover: int_array_4;
+                heater: int_array_4;
+                motor_failed: int_array_4;
+                rain: int_array_4;
+                reset_needed: int_array_4;
+                router: int_array_4;
+                spectrometer: int_array_4;
+                ups_alert: int_array_4;
+            };
+        };
         vbdsd: {
-            sensor_is_present: boolean;
             cam_id: number;
             image_storage_path: string;
+            sensor_is_present: boolean;
+        };
+    };
+
+    export type parametersJSON = {
+        camtracker: {
+            motor_offset_treshold: number;
+        };
+        em27: {
+            power_min_angle: number;
+        };
+        opus: {
+            executable_parameter: string;
+            experiment_path: string;
+            macro_path: string;
+        };
+        pyra: {
+            automation_is_running: boolean;
+            seconds_per_iteration: number;
+            test_mode: boolean;
+        };
+        vbdsd: {
+            evaluation_size: number;
+            interval_time: number;
+            measurement_threshold: number;
+            min_sun_angle: number;
+        };
+        enclosure: {
+            continuous_readings: any[];
+            min_sun_angle: number;
+        };
+        measurement_triggers: {
+            type: {
+                time: boolean;
+                sun_angle: boolean;
+                vbdsd: boolean;
+                user_control: boolean;
+            };
+            start_time: int_array_3;
+            stop_time: int_array_3;
+            user_trigger_present: boolean;
+            sun_angle_start: number;
+            sun_angle_stop: number;
+        };
+        measurement_conditions: {
+            current_sun_angle: number;
         };
     };
 }
