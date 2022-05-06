2d25b3ebead938edcde4f3f15d3d53b57d5bffc2
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-06 20:27:47 +0200

    pyra-ui - Add collapsing mechanism to config sections

--- packages/ui/src/client/assets/icons.tsx
+++ packages/ui/src/client/assets/icons.tsx
@@ -24,6 +24,15 @@ const ICONS = {
             />
         </SVG>
     ),
+    chevronDown: (
+        <SVG>
+            <path
+                className='secondary'
+                fillRule='evenodd'
+                d='M15.3 10.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z'
+            />
+        </SVG>
+    ),
 };
 
 export default ICONS;
--- packages/ui/src/client/components/config-section.tsx
+++ packages/ui/src/client/components/config-section.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ICONS from '../assets/icons';
 import TYPES from '../../types/index';
 
 import TextInputRow from './text-input-row';
@@ -12,42 +13,64 @@ export default function ConfigSection(props: {
     addLocalUpdate(v: TYPES.configJSON): void;
 }) {
     const { key1, localJSON, centralJSON, addLocalUpdate } = props;
+    const [open, setOpen] = useState(false);
+
     return (
         <>
-            {Object.keys(centralJSON[key1]).map((key2: string, j: number) => {
-                const commonProps = {
-                    key: `${key1}.${key2}`,
-                    label: `${key1}.${key2}`,
-                    value: localJSON[key1][key2],
-                    oldValue: centralJSON[key1][key2],
-                    setValue: (v: any) =>
-                        addLocalUpdate({
-                            [key1]: {
-                                [key2]: v,
-                            },
-                        }),
-                };
-                switch (typeof centralJSON[key1][key2]) {
-                    case 'string':
-                        return (
-                            /* @ts-ignore */
-                            <TextInputRow
-                                {...commonProps}
-                                showfileSelector={key2.endsWith('_path')}
-                            />
-                        );
-                    case 'boolean':
-                        return (
-                            /* @ts-ignore */
-                            <ToggleRow {...commonProps} />
-                        );
-                    case 'object':
-                        return (
-                            /* @ts-ignore */
-                            <IntArrayMatrix {...commonProps} />
-                        );
-                }
-            })}
+            <div className='w-full flex-row-left gap-x-1'>
+                <button
+                    className={
+                        'w-7 h-7 p-0.5 rounded hover:bg-gray-50 ' +
+                        (open ? 'rotate-0 ' : '-rotate-90')
+                    }
+                    onClick={() => setOpen(!open)}
+                >
+                    {ICONS.chevronDown}
+                </button>
+                <h2 className='text-lg font-bold text-gray-800'>
+                    {key1.toLocaleUpperCase()}
+                </h2>
+            </div>
+
+            {open &&
+                Object.keys(centralJSON[key1]).map(
+                    (key2: string, j: number) => {
+                        const commonProps = {
+                            key: `${key1}.${key2}`,
+                            label: `${key1}.${key2}`,
+                            value: localJSON[key1][key2],
+                            oldValue: centralJSON[key1][key2],
+                            setValue: (v: any) =>
+                                addLocalUpdate({
+                                    [key1]: {
+                                        [key2]: v,
+                                    },
+                                }),
+                        };
+                        switch (typeof centralJSON[key1][key2]) {
+                            case 'string':
+                                return (
+                                    /* @ts-ignore */
+                                    <TextInputRow
+                                        {...commonProps}
+                                        showfileSelector={key2.endsWith(
+                                            '_path'
+                                        )}
+                                    />
+                                );
+                            case 'boolean':
+                                return (
+                                    /* @ts-ignore */
+                                    <ToggleRow {...commonProps} />
+                                );
+                            case 'object':
+                                return (
+                                    /* @ts-ignore */
+                                    <IntArrayMatrix {...commonProps} />
+                                );
+                        }
+                    }
+                )}
         </>
     );
 }
