49fb56db3b00c74b40e7fe66b212d7746ec1a758
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-07 21:25:19 +0200

    pyra-core - Move ring-list from vbdsd to utils

--- packages/core/modules/vbdsd.py
+++ packages/core/modules/vbdsd.py
@@ -36,67 +36,15 @@ import astropy.coordinates as astropy_coordinates
 import astropy.time as astropy_time
 import cv2 as cv
 import numpy as np
-from packages.core.utils.json_file_interaction import PROJECT_DIR, State
+from packages.core.utils.json_file_interaction import State
 from packages.core.utils.logger import Logger
+from packages.core.utils.ring_list import RingList
 
 logger = Logger(origin="pyra.core.vbdsd")
 
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(dir(os.path.abspath(__file__)))))
 IMG_DIR = os.path.join(PROJECT_DIR, "runtime_data", " vbdsd")
-
-
-class RingList:
-    """Base code created by Flavio Catalani on Tue, 5 Jul 2005 (PSF).
-    Added sum() and reinitialize() functions.
-    """
-
-    def __init__(self, length):
-        self.__data__ = []
-        self.__full__ = 0
-        self.__max__ = length
-        self.__cur__ = 0
-
-    def append(self, x):
-        if self.__full__ == 1:
-            for i in range(0, self.__cur__ - 1):
-                self.__data__[i] = self.__data__[i + 1]
-            self.__data__[self.__cur__ - 1] = x
-        else:
-            self.__data__.append(x)
-            self.__cur__ += 1
-            if self.__cur__ == self.__max__:
-                self.__full__ = 1
-
-    def get(self):
-        return self.__data__
-
-    def remove(self):
-        if self.__cur__ > 0:
-            del self.__data__[self.__cur__ - 1]
-            self.__cur__ -= 1
-
-    def size(self):
-        return self.__cur__
-
-    def maxsize(self):
-        return self.__max__
-
-    def sum(self):
-        return float(sum(self.get()))
-
-    def reinitialize(self, length):
-        self.__max__ = length
-        self.__full__ = 0
-        self.__cur__ = 0
-        handover_list = self.get()
-        self.__data__ = []
-
-        for item in handover_list:
-            self.append(item)
-
-    def __str__(self):
-        return "".join(self.__data__)
 
 
 def init_cam(cam_id):
