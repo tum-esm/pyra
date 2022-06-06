f6f663a1878a56a5f92fbe4277a8e990eaeb635d
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-06-06 14:19:32 +0200

    pyra-cli - extend pytest fixtures

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,33 +5,69 @@ import pytest
 
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(os.path.abspath(__file__)))
-CONFIG_FILE_PATH = os.path.join(PROJECT_DIR, "config", "config.json")
-CONFIG_FILE_PATH_TMP = os.path.join(PROJECT_DIR, "config", "config.original.json")
+
+CONFIG_FILE_PATHS = {
+    "actual": os.path.join(PROJECT_DIR, "config", "config.json"),
+    "temporary": os.path.join(PROJECT_DIR, "config", "config.original.json"),
+}
+INFO_LOG_PATHS = {
+    "actual": os.path.join(PROJECT_DIR, "logs", "info.log"),
+    "temporary": os.path.join(PROJECT_DIR, "logs", "info.original.log"),
+}
+DEBUG_LOGS_PATHS = {
+    "actual": os.path.join(PROJECT_DIR, "logs", "debug.log"),
+    "temporary": os.path.join(PROJECT_DIR, "logs", "debug.original.log"),
+}
+LOG_PATHSS = [INFO_LOG_PATHS, DEBUG_LOGS_PATHS]
+
+
+def save_file(paths: dict):
+    if os.path.isfile(paths["temporary"]):
+        os.remove(paths["temporary"])
+    shutil.copy(paths["actual"], paths["temporary"])
+
+
+def restore_file(paths: dict):
+    if os.path.isfile(paths["actual"]):
+        os.remove(paths["actual"])
+    assert os.path.isfile(
+        paths["temporary"]
+    ), f'{paths["temporary"]} does not exist anymore'
+    os.rename(paths["temporary"], paths["actual"])
 
 
 @pytest.fixture()
 def original_config():
     # save original config.json for later
-    assert os.path.isfile(CONFIG_FILE_PATH), "config.json does not exist"
-    assert not os.path.isfile(
-        CONFIG_FILE_PATH_TMP
-    ), "config.original.json already exist"
-
+    assert os.path.isfile(CONFIG_FILE_PATHS["actual"]), "config.json does not exist"
     try:
-        with open(CONFIG_FILE_PATH, "r") as f:
+        with open(CONFIG_FILE_PATHS["actual"], "r") as f:
             config = json.load(f)
     except json.JSONDecodeError:
         raise Exception("config.json is invalid")
 
-    shutil.copy(CONFIG_FILE_PATH, CONFIG_FILE_PATH_TMP)
+    # save original config.json for later
+    save_file(CONFIG_FILE_PATHS)
 
     # run the respective test
     yield config
 
     # restore original config.json
-    if os.path.isfile(CONFIG_FILE_PATH):
-        os.remove(CONFIG_FILE_PATH)
-    assert os.path.isfile(
-        CONFIG_FILE_PATH_TMP
-    ), "config.original.json does not exist anymore"
-    os.rename(CONFIG_FILE_PATH_TMP, CONFIG_FILE_PATH)
+    restore_file(CONFIG_FILE_PATHS)
+
+
+@pytest.fixture()
+def original_log_files():
+    # save original logs for later
+    for log_paths in LOG_PATHSS:
+        if not os.path.isfile(log_paths["actual"]):
+            with open(log_paths["actual"], "w") as f:
+                pass
+        save_file(log_paths)
+
+    # run the respective test
+    yield
+
+    # restore original config.json
+    for log_paths in LOG_PATHSS:
+        restore_file(log_paths)
