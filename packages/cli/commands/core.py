a9537f5d63dcff08c756eb7188295c373b1c4895
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-06-06 14:58:00 +0200

    pyra-cli - improve core starting/stopping

--- packages/cli/commands/core.py
+++ packages/cli/commands/core.py
@@ -7,8 +7,6 @@ import psutil
 
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(dir(os.path.abspath(__file__)))))
-PROCESS_STATE_FILE = os.path.join(PROJECT_DIR, "pyra-core-process-state.json")
-
 INTERPRETER_PATH = os.path.join(PROJECT_DIR, ".venv", "bin", "python")
 SCRIPT_PATH = os.path.join(PROJECT_DIR, "run-pyra-core.py")
 
@@ -16,44 +14,61 @@ error_handler = lambda text: click.echo(click.style(text, fg="red"))
 success_handler = lambda text: click.echo(click.style(text, fg="green"))
 
 
+def process_is_running():
+    for p in psutil.process_iter():
+        try:
+            arguments = p.cmdline()
+            if (len(arguments) > 0) and (arguments[-1] == SCRIPT_PATH):
+                return p.pid
+        except (psutil.AccessDenied, psutil.ZombieProcess, psutil.NoSuchProcess):
+            pass
+    return None
+
+
+def terminate_processes():
+    termination_pids = []
+    for p in psutil.process_iter():
+        try:
+            arguments = p.cmdline()
+            if (len(arguments) > 0) and (arguments[-1] == SCRIPT_PATH):
+                termination_pids.append(p.pid)
+                p.terminate()
+        except (psutil.AccessDenied, psutil.ZombieProcess, psutil.NoSuchProcess):
+            pass
+    return termination_pids
+
+
 @click.command(
     help="Start pyra-core as a background process. "
     + "Prevents spawning multiple processes"
 )
 def _start_pyra_core():
-    if os.path.isfile(PROCESS_STATE_FILE):
-        error_handler("Background process already exists")
+    existing_pid = process_is_running()
+    if existing_pid is not None:
+        error_handler(f"Background process already exists with PID {existing_pid}")
     else:
-        p = subprocess.Popen([INTERPRETER_PATH, SCRIPT_PATH])
-        success_handler(f"Started pyra-core with PID {p.pid}")
+        p = subprocess.Popen([INTERPRETER_PATH, SCRIPT_PATH], stderr=subprocess.PIPE)
+        success_handler(f"Started background process with PID {p.pid}")
 
 
 @click.command(help="Stop the pyra-core background process")
 def _stop_pyra_core():
-    if not os.path.isfile(PROCESS_STATE_FILE):
-        error_handler("No background process found")
+    termination_pids = terminate_processes()
+    if len(termination_pids) == 0:
+        error_handler("No active process to be terminated")
     else:
-        try:
-            with open(PROCESS_STATE_FILE, "r") as f:
-                pid = json.load(f)["pid"]
-            try:
-                psutil.Process(pid).terminate()
-                success_handler(f"Terminated background process with PID {pid}")
-            except psutil.NoSuchProcess:
-                error_handler("Process has already been terminated")
-            os.remove(PROCESS_STATE_FILE)
-        except Exception as e:
-            error_handler(f"Could not terminate background process: {e}")
+        success_handler(
+            f"Terminated {len(termination_pids)} background "
+            + f"processes with PIDs {termination_pids}"
+        )
 
 
 @click.command(help="Checks whether the pyra-core background process is running")
 def _pyra_core_is_running():
-    try:
-        with open(PROCESS_STATE_FILE, "r") as f:
-            pid = json.load(f)["pid"]
-        assert psutil.Process(pid).status() == psutil.STATUS_RUNNING
-        success_handler(f"pyra-core is running with PID {pid}")
-    except:
+    existing_pid = process_is_running()
+    if existing_pid is not None:
+        success_handler(f"pyra-core is running with PID {existing_pid}")
+    else:
         error_handler("pyra-core is not running")
 
 
--- run-pyra-core.py
+++ run-pyra-core.py
@@ -1,19 +1,4 @@
-import json
-import os
-import sys
 from packages.core import main
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-PROCESS_STATE_FILE = os.path.join(PROJECT_DIR, "pyra-core-process-state.json")
-
-# process is already running/has not been terminated correctly
-if os.path.exists(PROCESS_STATE_FILE):
-    sys.exit()
-
-with open(PROCESS_STATE_FILE, "w") as f:
-    json.dump({"pid": os.getpid()}, f)
-
-try:
+if __name__ == "__main__":
     main.run()
-except:
-    os.remove(PROCESS_STATE_FILE)
