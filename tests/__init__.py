0950dfb4bcb2b1cac8d4828d32a62448621033bf
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-06-06 12:47:06 +0200

    pyra-cli - improve config tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 import pytest
@@ -5,21 +6,32 @@ import pytest
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(os.path.abspath(__file__)))
 CONFIG_FILE_PATH = os.path.join(PROJECT_DIR, "config", "config.json")
-CONFIG_FILE_PATH_TMP = os.path.join(PROJECT_DIR, "config", "config.tmp.json")
+CONFIG_FILE_PATH_TMP = os.path.join(PROJECT_DIR, "config", "config.original.json")
 
 
 @pytest.fixture()
-def config_file():
-    # save config.json in another file
-    assert os.path.isfile(CONFIG_FILE_PATH)
-    assert not os.path.isfile(CONFIG_FILE_PATH_TMP)
+def original_config():
+    # save original config.json for later
+    assert os.path.isfile(CONFIG_FILE_PATH), "config.json does not exist"
+    assert not os.path.isfile(
+        CONFIG_FILE_PATH_TMP
+    ), "config.original.json already exist"
+
+    try:
+        with open(CONFIG_FILE_PATH, "r") as f:
+            config = json.load(f)
+    except json.JSONDecodeError:
+        raise Exception("config.json is invalid")
+
     shutil.copy(CONFIG_FILE_PATH, CONFIG_FILE_PATH_TMP)
 
     # run the respective test
-    yield
+    yield config
 
-    # restore old config.json
+    # restore original config.json
     if os.path.isfile(CONFIG_FILE_PATH):
         os.remove(CONFIG_FILE_PATH)
-    assert os.path.isfile(CONFIG_FILE_PATH_TMP)
+    assert os.path.isfile(
+        CONFIG_FILE_PATH_TMP
+    ), "config.original.json does not exist anymore"
     os.rename(CONFIG_FILE_PATH_TMP, CONFIG_FILE_PATH)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,18 +2,40 @@ import json
 import subprocess
 import os
 from deepdiff import DeepDiff
-from . import config_file
+from . import original_config
 
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(os.path.abspath(__file__)))
 INTERPRETER_PATH = os.path.join(PROJECT_DIR, ".venv", "bin", "python")
 PYRA_CLI_PATH = os.path.join(PROJECT_DIR, "packages", "cli", "main.py")
-
 CONFIG_FILE_PATH = os.path.join(PROJECT_DIR, "config", "config.json")
-CONFIG_FILE_PATH_TMP = os.path.join(PROJECT_DIR, "config", "config.tmp.json")
 
 
-def test_get_config(config_file):
+def assert_config_file_content(expected_content: dict, message: str):
+    with open(CONFIG_FILE_PATH, "r") as f:
+        actual_content = json.load(f)
+
+    difference = DeepDiff(expected_content, actual_content)
+    assert difference == {}, f"{message}: {difference}"
+
+
+def assert_current_config_validity(verbose: bool = False):
+    process = subprocess.run(
+        [INTERPRETER_PATH, PYRA_CLI_PATH, "config", "validate"],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    stdout = process.stdout.decode()
+    stderr = process.stderr.decode()
+    if verbose:
+        print(f"stdout: {stdout}")
+        print(f"stderr: {stderr}")
+
+    assert process.returncode == 0
+    assert stdout.startswith("Current config file is valid")
+
+
+def test_get_config(original_config):
     # get config from cli
     process = subprocess.run(
         [INTERPRETER_PATH, PYRA_CLI_PATH, "config", "get"],
@@ -29,25 +51,19 @@ def test_get_config(config_file):
     config_object_1 = json.loads(stdout)
 
     # get config from file
-    with open(CONFIG_FILE_PATH, "r") as f:
-        config_object_2 = json.load(f)
-
-    # assert equality
-    difference = DeepDiff(config_object_1, config_object_2)
-    assert difference == {}, difference
+    assert_config_file_content(
+        config_object_1, "output from cli does not match file content"
+    )
 
 
-"""
-def test_validate_current_config():
-    # TODO: run "pyra-cli config validate"
-    # TODO: Assert success
-    pass
+def test_validate_current_config(original_config):
+    assert_current_config_validity(verbose=True)
 
 
-def test_set_config():
+def test_set_config(original_config):
     # TODO: run "pyra-cli config set" for some invalid variables
     # TODO: Assert failure and unchanged JSON file inbetween
+
     # TODO: run "pyra-cli config set" for some valid variables
     # TODO: Assert success and changed JSON file inbetween
     pass
-"""
