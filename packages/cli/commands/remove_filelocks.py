f25001e7a8b6cfbc02aa0141da5b4c77fa96175b
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-06-14 02:56:34 +0200

    pyra-cli - Add "remove-lockfiles" command

---
+++ packages/cli/commands/remove_filelocks.py
@@ -0,0 +1,31 @@
+import click
+import os
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(dir(dir(os.path.abspath(__file__)))))
+STATE_FILE_PATH = os.path.join(PROJECT_DIR, "runtime-data", "state.json")
+STATE_LOCK_PATH = os.path.join(PROJECT_DIR, "runtime-data", ".state.lock")
+
+
+error_handler = lambda text: click.echo(click.style(text, fg="red"))
+success_handler = lambda text: click.echo(click.style(text, fg="green"))
+
+
+@click.command(
+    help="Remove all filelocks. Helpful when any of the programs crashed "
+    + "during writing to a file. Should not be necessary normally."
+)
+def remove_filelocks():
+    lock_files = [
+        os.path.join(PROJECT_DIR, "config", ".config.lock"),
+        os.path.join(PROJECT_DIR, "logs", ".logs.lock"),
+        os.path.join(PROJECT_DIR, "state", ".state.lock"),
+    ]
+    if input("Are you sure? (y) ").startswith("y"):
+        for f in lock_files:
+            if os.path.isfile(f):
+                os.remove(f)
+                success_handler(f"Removing {f}")
+        success_handler("Done!")
+    else:
+        error_handler("Aborting")
--- packages/cli/main.py
+++ packages/cli/main.py
@@ -10,6 +10,7 @@ from packages.cli.commands.config import config_command_group
 from packages.cli.commands.state import state_command_group
 from packages.cli.commands.logs import logs_command_group
 from packages.cli.commands.core import core_command_group
+from packages.cli.commands.remove_filelocks import remove_filelocks
 
 
 @click.group()
@@ -21,6 +22,7 @@ cli.add_command(config_command_group, name="config")
 cli.add_command(state_command_group, name="state")
 cli.add_command(logs_command_group, name="logs")
 cli.add_command(core_command_group, name="core")
+cli.add_command(remove_filelocks, name="remove-filelocks")
 
 
 if __name__ == "__main__":
