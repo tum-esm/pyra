a89603cbbabcd56dbee0adb12835cc31e563d4ef
patrickaigner <pataigner.login@gmail.com>
2022-06-24 11:43:28 +0200

    new reset testcase

---
+++ tests/test_enclosure_reset_check.py
@@ -0,0 +1,14 @@
+from packages.core.utils import ConfigInterface, OSInfo, STANDARD_PLC_INTERFACES, PLCInterface
+from packages.core.modules.enclosure_control import EnclosureControl
+
+import time
+
+def test_cover_movement():
+    _CONFIG = ConfigInterface().read()
+    _PLC_INTERFACE: PLCInterface = STANDARD_PLC_INTERFACES[_CONFIG["tum_plc"]["version"]]
+    enclosure = EnclosureControl(_CONFIG)
+
+    state = enclosure.check_for_rest_needed()
+    print(state)
+
+    assert(False)
