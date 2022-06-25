00cb163794dc9e42229182f5eedbe6b7bcfbb53e
ESM Technical User <esm-technical-user@protonmail.com>
2022-06-25 13:03:22 +0200

    vbdsd_test is now functional

--- tests/test_vbdsd.py
+++ tests/test_vbdsd.py
@@ -1,8 +1,9 @@
-from packages.core.modules import vbdsd
+from packages.core.modules.vbdsd import VBDSD_Thread
 
     
 def test_vbdsd():
-    vbdsd = vbdsd.VBDSD_Thread()
+    """Pictures are saved in C:\pyra-4\runtime-data\vbdsd"""
+    vbdsd = VBDSD_Thread()
     vbdsd.main(infinite_loop=False)
-    assert False
+    
 
