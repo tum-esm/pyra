e869722deed3478e7bb16e02263dd603d081a469
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-06-27 02:56:59 +0200

    pyra-ui - Add overview tab placeholder

--- packages/ui/src/tabs/overview-tab.tsx
+++ packages/ui/src/tabs/overview-tab.tsx
@@ -1,3 +1,13 @@
 export default function OverviewTab() {
-    return <div className={'flex-col w-full h-full overflow-y-scroll gap-y-4 py-4 '}>banger</div>;
+    return (
+        <div className={'flex-col-center w-full h-full overflow-y-scroll gap-y-4 py-4 px-6'}>
+            <div>pyra core state </div>
+            <div>measurement state </div>
+            <div className="w-full h-px bg-gray-300" />
+            <div>plc readings, force close cover button </div>
+            <div>system stats</div>
+            <div className="w-full h-px bg-gray-300" />
+            <div>last iterations info logs</div>
+        </div>
+    );
 }
