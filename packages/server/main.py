3318096ee1de19ef5493261a85802ca5edc87e00
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-07-08 02:15:06 +0200

    Improve log lines design

--- packages/server/main.py
+++ packages/server/main.py
@@ -1,6 +1,4 @@
 import os
-from threading import Thread
-from flask_cors import CORS
 import socketio
 from flask import Flask
 
@@ -12,10 +10,8 @@ sio = socketio.Server(async_mode="threading", cors_allowed_origins="*")
 app = Flask(__name__)
 app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 
-CORS(app)
-
-# When the server receives a "new_log_line"/"new_core_state" event, it broadcasts
-# the event back to all connected clients that registered as pyra-ui clients.
+# When the server receives a "new_log_line"/"new_core_state" event,
+# it broadcasts the event back to all connected clients
 
 
 @sio.event
--- packages/ui/src/tabs/log-tab.tsx
+++ packages/ui/src/tabs/log-tab.tsx
@@ -3,6 +3,51 @@ import { dialog, shell } from '@tauri-apps/api';
 import { fetchUtils, reduxUtils } from '../utils';
 import { essentialComponents } from '../components';
 import toast from 'react-hot-toast';
+import { drop } from 'lodash';
+
+function RenderedLogLine(props: { l: string }) {
+    if (props.l.includes('EXCEPTION')) {
+        return <pre className="font-bold text-red-700">{props.l}</pre>;
+    }
+
+    let l = props.l.replace(/\n/g, '');
+
+    try {
+        const timeSection = l.slice(11, 26);
+        const sections = l.slice(29, undefined).split(' - ');
+        const sourceSection = sections[0];
+        const typeSection = sections[1];
+        const messageSection = drop(sections, 2).join(' - ');
+
+        let textStyle = 'text-gray-500 font-light';
+        switch (typeSection) {
+            case 'INFO':
+                textStyle = 'text-gray-900 font-semibold';
+                break;
+            case 'WARNING':
+            case 'CRITICAL':
+            case 'ERROR':
+                textStyle = 'text-red-700 font-bold';
+                break;
+        }
+
+        return (
+            <>
+                {messageSection.includes('Starting Iteration') && (
+                    <div className="flex-shrink-0 h-px my-1 bg-gray-250 w-[calc(100%+2rem)] -ml-4 first:hidden" />
+                )}
+                <div className={`flex-row-left gap-x-3 ${textStyle} whitespace-nowrap mr-3`}>
+                    <div>{timeSection}</div>
+                    <div className="flex-shrink-0 w-44">{sourceSection}</div>
+                    <div className="flex-shrink-0 w-12">{typeSection}</div>
+                    <div>{messageSection}</div>
+                </div>
+            </>
+        );
+    } catch {
+        return <></>;
+    }
+}
 
 export default function LogTab() {
     const [logLevel, setLogLevel] = useState<'info' | 'debug'>('info');
@@ -53,13 +98,15 @@ export default function LogTab() {
             </div>
             <pre
                 className={
-                    'w-full !px-6 !py-2 !mb-0 overflow-y-scroll ' +
+                    'w-full !px-4 !py-2 !mb-0 overflow-y-scroll ' +
                     'border-t border-gray-300 bg-white flex-grow'
                 }
             >
                 {!logsAreLoading && (
                     <code className="w-full h-full !text-xs language-log">
-                        {(logLevel === 'info' ? infoLogLines : debugLogLines).join('\n')}
+                        {(logLevel === 'info' ? infoLogLines : debugLogLines).map((l) => (
+                            <RenderedLogLine l={l} key={l} />
+                        ))}
                         {debugLogLines.length == 0 && <strong>logs are empty</strong>}
                     </code>
                 )}
