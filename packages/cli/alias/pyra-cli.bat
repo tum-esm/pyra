a2ca1c4b9ba6d8363099af21fcbaf56289746d8e
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-08-01 20:04:56 +0200

    Apply changes to cli .bat-file in mb02 branch

--- packages/cli/alias/pyra-cli.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo.
-python C:\Users\EnclosureMb02\Documents\pyra-4\packages\cli\main.py %
---
+++ packages/cli/alias/pyra-cli.example.bat
@@ -0,0 +1,3 @@
+@echo off
+echo.
+python C:\Users\...\Documents\pyra-4\packages\cli\main.py %
