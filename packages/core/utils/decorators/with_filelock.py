d104fcf473c404de3935fb16f96bb3f1053c1f24
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-08-19 17:17:57 +0200

    #91 - document decorators

--- packages/core/utils/decorators/with_filelock.py
+++ packages/core/utils/decorators/with_filelock.py
@@ -2,29 +2,23 @@ import filelock
 from typing import Any, Callable, TypeVar, cast
 from functools import wraps
 
-# FileLock = Mark, that a file is being used and other programs
-# should not interfere. A file "*.lock" will be created and the
-# existence of this file will make the wrapped function wait until
-# it no longer exists.
-
-# A timeout of -1 means that the code waits forever
-
-# def with_filelock(file_lock_path: str, timeout: float = -1):
-#     def with_fixed_filelock(f):
-#         def locked_function(*args, **kwargs):
-#             with filelock.FileLock(file_lock_path, timeout=timeout):
-#                 return function(*args, **kwargs)
-#         return locked_function
-#     return with_fixed_filelock
-#
 # typing of higher level decorators:
 # https://github.com/python/mypy/issues/1551#issuecomment-253978622
-
 F = TypeVar("F", bound=Callable[..., Any])
 
 
 class with_filelock:
+    """
+    FileLock = Mark, that a file is being used and other programs
+    should not interfere. A file "*.lock" will be created and the
+    content of this file will make the wrapped function possibly
+    wait until other programs are done using it.
+
+    See https://en.wikipedia.org/wiki/Semaphore_(programming)
+    """
+
     def __init__(self, file_lock_path: str, timeout: float = -1) -> None:
+        """A timeout of -1 means that the code waits forever"""
         self.file_lock_path: str = file_lock_path
         self.timeout: float = timeout
 
