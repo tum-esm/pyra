f8a153fd74cbf9ef39889c612be722584180742d
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-08-24 03:19:54 +0200

    #100 - test default config

---
+++ tests/repository/test_default_config.py
@@ -0,0 +1,31 @@
+import json
+import os
+import sys
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
+CONFIG_DIR = os.path.join(PROJECT_DIR, "config")
+
+sys.path.append(PROJECT_DIR)
+from packages.core import types
+
+
+def test_default_config() -> None:
+    with open(os.path.join(CONFIG_DIR, "config.default.json"), "r") as f:
+        config = json.load(f)
+    types.validate_config_dict(config, skip_filepaths=True)
+
+    with open(os.path.join(CONFIG_DIR, "tum_plc.config.default.json"), "r") as f:
+        config_tum_plc = json.load(f)
+    config["tum_plc"] = config_tum_plc
+    types.validate_config_dict(config, skip_filepaths=True)
+
+    with open(os.path.join(CONFIG_DIR, "helios.config.default.json"), "r") as f:
+        config_helios = json.load(f)
+    config["helios"] = config_helios
+    types.validate_config_dict(config, skip_filepaths=True)
+
+    with open(os.path.join(CONFIG_DIR, "upload.config.default.json"), "r") as f:
+        config_upload = json.load(f)
+    config["upload"] = config_upload
+    types.validate_config_dict(config, skip_filepaths=True)
