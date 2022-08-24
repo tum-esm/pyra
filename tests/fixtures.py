3f57e76004eae7717a2968c604f7bb8816a25190
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-08-24 22:45:26 +0200

    #100 - add fixture for upload test directories

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,122 +1,169 @@
 import json
 import os
+import random
 import shutil
 import pytest
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 EXISTING_TEST_FILE_PATH = os.path.join(PROJECT_DIR, "pyproject.toml")
 
-CONFIG_FILE_PATHS = {
-    "actual": os.path.join(PROJECT_DIR, "config", "config.json"),
-    "temporary": os.path.join(PROJECT_DIR, "config", "config.original.json"),
-    "test_content": json.dumps(
-        {
-            "general": {
-                "seconds_per_core_interval": 30,
-                "test_mode": True,
-                "station_id": "...",
-                "min_sun_elevation": 11,
-            },
-            "opus": {
-                "em27_ip": "10.10.0.1",
-                "executable_path": EXISTING_TEST_FILE_PATH,
-                "experiment_path": EXISTING_TEST_FILE_PATH,
-                "macro_path": EXISTING_TEST_FILE_PATH,
-                "username": "Default",
-                "password": "...",
-            },
-            "camtracker": {
-                "config_path": EXISTING_TEST_FILE_PATH,
-                "executable_path": EXISTING_TEST_FILE_PATH,
-                "learn_az_elev_path": EXISTING_TEST_FILE_PATH,
-                "sun_intensity_path": EXISTING_TEST_FILE_PATH,
-                "motor_offset_threshold": 10,
-            },
-            "error_email": {
-                "sender_address": "pyra.technical.user@gmail.com",
-                "sender_password": "...",
-                "notify_recipients": True,
-                "recipients": "your@mail.com",
-            },
-            "measurement_decision": {
-                "mode": "automatic",
-                "manual_decision_result": False,
-                "cli_decision_result": False,
-            },
-            "measurement_triggers": {
-                "consider_time": True,
-                "consider_sun_elevation": True,
-                "consider_helios": False,
-                "start_time": {"hour": 7, "minute": 0, "second": 0},
-                "stop_time": {"hour": 21, "minute": 0, "second": 0},
-                "min_sun_elevation": 0,
-            },
-            "tum_plc": None,
-            "helios": None,
-            "upload": None,
-        },
-        indent=4,
-    ),
+TEST_CONFIG = {
+    "general": {
+        "seconds_per_core_interval": 30,
+        "test_mode": True,
+        "station_id": "...",
+        "min_sun_elevation": 11,
+    },
+    "opus": {
+        "em27_ip": "10.10.0.1",
+        "executable_path": EXISTING_TEST_FILE_PATH,
+        "experiment_path": EXISTING_TEST_FILE_PATH,
+        "macro_path": EXISTING_TEST_FILE_PATH,
+        "username": "Default",
+        "password": "...",
+    },
+    "camtracker": {
+        "config_path": EXISTING_TEST_FILE_PATH,
+        "executable_path": EXISTING_TEST_FILE_PATH,
+        "learn_az_elev_path": EXISTING_TEST_FILE_PATH,
+        "sun_intensity_path": EXISTING_TEST_FILE_PATH,
+        "motor_offset_threshold": 10,
+    },
+    "error_email": {
+        "sender_address": "pyra.technical.user@gmail.com",
+        "sender_password": "...",
+        "notify_recipients": True,
+        "recipients": "your@mail.com",
+    },
+    "measurement_decision": {
+        "mode": "automatic",
+        "manual_decision_result": False,
+        "cli_decision_result": False,
+    },
+    "measurement_triggers": {
+        "consider_time": True,
+        "consider_sun_elevation": True,
+        "consider_helios": False,
+        "start_time": {"hour": 7, "minute": 0, "second": 0},
+        "stop_time": {"hour": 21, "minute": 0, "second": 0},
+        "min_sun_elevation": 0,
+    },
+    "tum_plc": None,
+    "helios": None,
+    "upload": None,
 }
-INFO_LOGS_PATHS = {
-    "actual": os.path.join(PROJECT_DIR, "logs", "info.log"),
-    "temporary": os.path.join(PROJECT_DIR, "logs", "info.original.log"),
-    "test_content": "",
-}
-DEBUG_LOGS_PATHS = {
-    "actual": os.path.join(PROJECT_DIR, "logs", "debug.log"),
-    "temporary": os.path.join(PROJECT_DIR, "logs", "debug.original.log"),
-    "test_content": "",
-}
+
+
+def save_file(original_path: str, temporary_path: str, test_content: str) -> None:
+    assert not os.path.exists(temporary_path)
+
+    try:
+        os.rename(original_path, temporary_path)
+    except FileNotFoundError:
+        pass
+
+    with open(original_path, "w") as f:
+        f.write(test_content)
+
+
+def restore_file(original_path: str, temporary_path: str):
+    os.remove(original_path)
+    try:
+        os.rename(temporary_path, original_path)
+    except FileNotFoundError:
+        pass
 
 
 @pytest.fixture()
 def original_config():
-    # save original config.json for later
-    assert not os.path.isfile(CONFIG_FILE_PATHS["temporary"])
-    try:
-        shutil.copyfile(CONFIG_FILE_PATHS["actual"], CONFIG_FILE_PATHS["temporary"])
-    except FileNotFoundError:
-        pass
+    """
+    Store the original config.json file under a different name.
+    Restore it after the tests are done.
+    """
 
-    # create temporary file
-    with open(CONFIG_FILE_PATHS["actual"], "w") as f:
-        f.write(CONFIG_FILE_PATHS["test_content"])
+    original_config_path = os.path.join(PROJECT_DIR, "config", "config.json")
+    temporary_config_path = os.path.join(PROJECT_DIR, "config", "config.tmp.json")
+    test_content = json.dumps(TEST_CONFIG, indent=4)
+    save_file(original_config_path, temporary_config_path, test_content)
 
     # run the respective test
-    yield json.loads(CONFIG_FILE_PATHS["test_content"])
+    yield TEST_CONFIG
 
-    # restore original config.json
-    os.remove(CONFIG_FILE_PATHS["actual"])
-    try:
-        os.rename(CONFIG_FILE_PATHS["temporary"], CONFIG_FILE_PATHS["actual"])
-    except FileNotFoundError:
-        pass
+    restore_file(original_config_path, temporary_config_path)
 
 
 @pytest.fixture()
 def original_logs():
-    assert not os.path.isfile(INFO_LOGS_PATHS["temporary"])
-    assert not os.path.isfile(DEBUG_LOGS_PATHS["temporary"])
+    """
+    Store the original info.log and debug.log file under a
+    different name. Restore them after the tests are done.
+    """
+    original_info_logs_path = os.path.join(PROJECT_DIR, "logs", "info.log")
+    temporary_info_logs_path = os.path.join(PROJECT_DIR, "logs", "info.tmp.log")
+    save_file(original_info_logs_path, temporary_info_logs_path, "")
 
-    # save original log files for later
-    for ps in [INFO_LOGS_PATHS, DEBUG_LOGS_PATHS]:
-        try:
-            shutil.copyfile(ps["actual"], ps["temporary"])
-        except FileNotFoundError:
-            pass
-
-        # create temporary file
-        with open(ps["actual"], "w") as f:
-            f.write(ps["test_content"])
+    original_debug_logs_path = os.path.join(PROJECT_DIR, "logs", "debug.log")
+    temporary_debug_logs_path = os.path.join(PROJECT_DIR, "logs", "debug.tmp.log")
+    save_file(original_debug_logs_path, temporary_debug_logs_path, "")
 
     # run the respective test
     yield
 
-    # restore original log files
-    for ps in [INFO_LOGS_PATHS, DEBUG_LOGS_PATHS]:
-        os.remove(ps["actual"])
-        try:
-            os.rename(ps["temporary"], ps["actual"])
-        except FileNotFoundError:
-            pass
+    restore_file(original_info_logs_path, temporary_info_logs_path)
+    restore_file(original_debug_logs_path, temporary_debug_logs_path)
+
+
+@pytest.fixture
+def populated_upload_test_directories():
+    """
+    Store the content of the logs/helios directory under a
+    different name
+    """
+    original_helios_dir = os.path.join(PROJECT_DIR, "logs", "helios")
+    temporary_helios_dir = os.path.join(PROJECT_DIR, "logs", "helios-tmp")
+    assert not os.path.exists(temporary_helios_dir)
+
+    # replace original helios dir with empty test dir
+    os.rename(original_helios_dir, temporary_helios_dir)
+    os.mkdir(original_helios_dir)
+    with open(os.path.join(original_helios_dir, ".gitkeep"), "w") as f:
+        f.write("")
+
+    # add a second test dir (ifg upload)
+    test_ifg_dir = os.path.join(PROJECT_DIR, "test-tmp")
+    assert not os.path.exists(test_ifg_dir)
+    os.mkdir(test_ifg_dir)
+
+    # add a bunch of random sample files to these directories
+    popuplate_upload_test_directory(original_helios_dir)
+    popuplate_upload_test_directory(test_ifg_dir)
+
+    # run the respective test
+    yield test_ifg_dir
+
+    # fill helios dir with original content again
+    shutil.rmtree(original_helios_dir)
+    os.rename(temporary_helios_dir, original_helios_dir)
+    shutil.rmtree(test_ifg_dir)
+
+
+def random_string() -> str:
+    letters = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+    return "".join([random.choice(letters) for i in range(5)])
+
+
+def popuplate_upload_test_directory(dir_path: str):
+    """
+    Generates 5 directories with random dates (YYYYMMDD)
+    each containing 10 files with random names
+    """
+    for _ in range(5):
+        year = random.randint(2000, 2022)
+        month = random.randint(1, 12)
+        day = random.randint(1, 28)
+        date_string = f"{year}{str(month).zfill(2)}{str(day).zfill(2)}"
+        os.mkdir(os.path.join(dir_path, date_string))
+        for _ in range(10):
+            filename = f"{random_string()}{date_string}{random_string()}"
+            with open(os.path.join(dir_path, date_string, filename), "w") as f:
+                f.write(random_string())
