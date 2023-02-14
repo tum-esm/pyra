5e9413816be18777a10fb5e3587aab74585316d4
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-14 01:56:27 +0100

    Refine static type checks script

---
+++ scripts/check_static_types.sh
@@ -0,0 +1,10 @@
+set -o errexit
+
+echo "Removing old mypy cache"
+rm -rf .mypy_cache 
+
+echo "Checking run-pyra-core.py"
+python -m mypy run-pyra-core.py
+
+echo "Checking packages/cli/main.py"
+python -m mypy packages/cli/main.py
--- scripts/run_type_analysis.sh
+++
@@ -1,7 +0,0 @@
-set -o errexit
-
-echo "running static type analysis for PYRA Core"
-python -m mypy run-pyra-core.py
-
-echo "running static type analysis for PYRA CLI"
-python -m mypy packages/cli/main.py
