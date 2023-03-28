5a92b6cbb36936ab983296d97569d9c3c9c01f9c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-03-28 18:56:02 +0200

    Debug CI, finally resolve caching

--- scripts/check_static_types.sh
+++ scripts/check_static_types.sh
@@ -6,10 +6,10 @@ echo "Removing old mypy cache"
 rm -rf .mypy_cache 
 
 echo "Checking run_pyra_core.py"
-python -m mypy run_pyra_core.py
+mypy run_pyra_core.py
 
 echo "Checking packages/cli/main.py"
-python -m mypy packages/cli/main.py
+mypy packages/cli/main.py
 
 echo "Checking tests/"
-python -m mypy tests/
+mypy tests/
