768ee247d4a6f5be91f332d2f020ff33eb13a43c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-02 23:17:54 +0200

    #154 (4)
    add astronomical dataset to repository
    remove astropy dependency

--- packages/core/utils/astronomy.py
+++ packages/core/utils/astronomy.py
@@ -1,20 +1,35 @@
-from typing import Optional
+import os
+from typing import Any, Optional
 import skyfield.api
+import tum_esm_utils
 from packages.core import types
 
-_PLANETS = skyfield.api.load("de421.bsp")
-_EARTH = _PLANETS["earth"]
-_SUN = _PLANETS["Sun"]
+
+PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=4)
 
 
 class Astronomy:
-    """Provides a method to compute the current sun elevation
-    based on the coordinates from the CamTracker config file."""
+    _PLANETS: Any = None
+
+    @staticmethod
+    def load_astronomical_dataset() -> None:
+        """Loads the astronomical dataset DE421 from the NASA JPL website,
+        see https://ssd.jpl.nasa.gov/planets/eph_export.html."""
+
+        filepath = os.path.join(PROJECT_DIR, "config", "astronomy_dataset_de421.bsp")
+        assert os.path.isfile(filepath), "Astronomical dataset not found"
+
+        if Astronomy._PLANETS is None:
+            Astronomy._PLANETS = skyfield.api.load_file(filepath)
 
     @staticmethod
     def get_current_sun_elevation(config: types.ConfigDict) -> float:
         """Computes current sun elevation in degree, based on the
         coordinates from the CamTracker config file."""
+
+        assert Astronomy._PLANETS is not None, "Astronomical dataset not loaded"
+        earth = Astronomy._PLANETS["Earth"]
+        sun = Astronomy._PLANETS["Sun"]
 
         with open(config["camtracker"]["config_path"], "r") as f:
             _lines = f.readlines()
@@ -31,12 +46,12 @@ class Astronomy:
         alt = float(_lines[_marker_line_index + 3].strip())
 
         current_time = skyfield.api.load.timescale().now()
-        current_position = _EARTH + skyfield.api.wgs84.latlon(
+        current_position = earth + skyfield.api.wgs84.latlon(
             latitude_degrees=lat,
             longitude_degrees=lon,
             elevation_m=alt,
         )
 
-        sun_pos = current_position.at(current_time).observe(_SUN).apparent()
+        sun_pos = current_position.at(current_time).observe(sun).apparent()
         altitude, _, _ = sun_pos.altaz()
         return float(altitude.degrees)
