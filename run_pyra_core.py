969cc3f69c5dd5f74a4c26d25459304ae2c991ba
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-03 16:27:35 +0200

    Close #156

--- run_pyra_core.py
+++ run_pyra_core.py
@@ -1,4 +1,22 @@
+import os
+import filelock
+import tum_esm_utils
 from packages.core import main
 
+_run_pyra_core_lock = filelock.FileLock(
+    os.path.join(
+        tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=2),
+        "run_pyra_core.lock",
+    ),
+    timeout=0.5,
+)
+
 if __name__ == "__main__":
-    main.run()
+    try:
+        _run_pyra_core_lock.acquire()
+        try:
+            main.run()
+        finally:
+            _run_pyra_core_lock.release()
+    except filelock.Timeout:
+        print("Aborting start: core process is already running")
