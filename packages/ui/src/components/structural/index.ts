d49f5199878207083636561df395984f261b76f8
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-10-23 20:10:33 +0200

    #175 Refine UI (14)
    Remove no longer used custom message queue design

--- packages/ui/src/components/structural/index.ts
+++ packages/ui/src/components/structural/index.ts
@@ -1,9 +1,7 @@
-import MessageQueue from './message-queue';
 import Header from './header';
 import DisconnectedScreen from './disconnected-screen';
 
 export default {
-    MessageQueue,
     Header,
     DisconnectedScreen,
 };
