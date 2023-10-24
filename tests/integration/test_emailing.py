7e8be459cd63d0993d461fb893a36b0e30f7486e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-10-24 20:38:54 +0200

    #109 Add button to send a test email (3)
    Only send test email in integration test

--- tests/integration/test_emailing.py
+++ tests/integration/test_emailing.py
@@ -5,9 +5,4 @@ from packages.core import utils, types
 @pytest.mark.integration
 def test_emailing() -> None:
     config = types.Config.load()
-
-    try:
-        raise Exception("some exception name")
-    except Exception as e:
-        utils.ExceptionEmailClient.handle_occured_exception(config, e)
-        utils.ExceptionEmailClient.handle_resolved_exception(config)
+    utils.ExceptionEmailClient.send_test_email(config)
