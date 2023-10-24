328bb522afe910610d90ac2f4a66b0b722d65994
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-10-24 17:11:34 +0200

    #190 Simplify state (2)
    Remove old state types

--- packages/core/types/__init__.py
+++ packages/core/types/__init__.py
@@ -3,6 +3,4 @@ from .config import Config, ConfigPartial, StrictIPAdress
 from .plc_specification import PlcSpecification
 from .state import PLCState, OperatingSystemState
 
-from .pyra_core_state import PyraCoreState, PyraCoreStatePersistent
-
 from .upload_meta import UploadMeta
