292643478b8fc0d6e72c06e9d153bf4ddb4d9137
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-10-26 23:09:49 +0200

    #193 Improve upload (10)
    Add integration test for upload

---
+++ tests/integration/test_uploading.py
@@ -0,0 +1,20 @@
+import circadian_scp_upload
+import pytest
+from packages.core import types
+
+
+@pytest.mark.integration
+def test_uploading() -> None:
+    config = types.Config.load()
+    if config.upload is None:
+        return
+
+    with circadian_scp_upload.RemoteConnection(
+        config.upload.host.root,
+        config.upload.user,
+        config.upload.password,
+    ) as remote_connection:
+        if remote_connection.connection.is_connected:
+            return
+
+    raise Exception("`circadian_scp_upload` should have raised an error")
