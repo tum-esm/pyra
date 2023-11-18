f80ebdccd3627689e24e6e1f4510b1c379bcc98d
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-18 12:56:39 +0100

    #204 Add button "test OPUS connection" (1)
    Add test command to CLI

--- packages/cli/commands/test.py
+++ packages/cli/commands/test.py
@@ -1,6 +1,6 @@
 import click
 import circadian_scp_upload
-from packages.core import interfaces, types, utils
+from packages.core import interfaces, types, utils, modules
 
 logger = utils.Logger(origin="cli")
 
@@ -18,12 +18,25 @@ def test_command_group() -> None:
     pass
 
 
+@test_command_group.command(name="opus")
+def _test_opus() -> None:
+    """Start OPUS, run a macro, stop the macro, close opus."""
+    interfaces.StateInterface.update_state(recent_cli_calls=1)
+    logger.info('running command "test opus"')
+    config = types.Config.load()
+    try:
+        modules.opus_measurement.OpusMeasurement(config).test_setup()
+    finally:
+        modules.opus_measurement.OpusMeasurement.force_kill_opus()
+    _print_green("Successfully tested opus connection.")
+
+
 @test_command_group.command(name="email")
 def _test_emailing() -> None:
     """Send a test email."""
     interfaces.StateInterface.update_state(recent_cli_calls=1)
     logger.info('running command "test email"')
-    config = types.Config.load(ignore_path_existence=True)
+    config = types.Config.load()
     utils.ExceptionEmailClient.send_test_email(config)
     _print_green("Successfully sent test email.")
 
@@ -33,7 +46,7 @@ def _test_uploading() -> None:
     """try to connect to upload server."""
     interfaces.StateInterface.update_state(recent_cli_calls=1)
     logger.info('running command "test upload"')
-    config = types.Config.load(ignore_path_existence=True)
+    config = types.Config.load()
     if config.upload is None:
         _print_red("No upload server configured.")
         return
