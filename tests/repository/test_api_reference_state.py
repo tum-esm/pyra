63bba584e7628399ea91d412b843eb4945984b9e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-12-18 13:11:21 +0100

    #209 Fix test for API reference

--- tests/repository/test_api_reference_state.py
+++ tests/repository/test_api_reference_state.py
@@ -1,24 +1,33 @@
 import os
+from typing import Generator
 import shutil
 import pytest
+import subprocess
 import tum_esm_utils
 
 PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=3)
-
 
 src = f"{PROJECT_DIR}/packages/docs/docs/api-reference"
 dst = f"{PROJECT_DIR}/packages/docs/docs/api-reference-backup"
 
 
-@pytest.mark.ci
-def test_api_reference_state() -> None:
+@pytest.fixture
+def setup() -> Generator[None, None, None]:
+    if os.path.isdir(dst):
+        shutil.rmtree(dst)
     shutil.copytree(src, dst)
-
-    tum_esm_utils.shell.run_shell_command(
-        "python scripts/update_api_reference.py", working_directory=PROJECT_DIR
-    )
-
-    a = os.system(f"diff --recursive {src} {dst}")
-    assert a == 0
-
+    yield
     shutil.rmtree(dst)
+
+
+@pytest.mark.ci
+def test_api_reference_state(setup: None) -> None:
+    tum_esm_utils.shell.run_shell_command(
+        "python scripts/update_api_reference.py",
+        working_directory=PROJECT_DIR,
+    )
+    stdout = tum_esm_utils.shell.run_shell_command(
+        f"diff --recursive {src} {dst}",
+        working_directory=PROJECT_DIR,
+    )
+    assert len(stdout) == 0
