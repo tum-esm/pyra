bb7a76a1501d03d561669596007244714a72de8d
Patrick <patrick.aigner@tum.de>
2024-07-15 16:01:59 +0200

    Add client connection test #225

--- tests/integration/test_thingsboard_connection.py
+++ tests/integration/test_thingsboard_connection.py
@@ -1,11 +1,21 @@
 import pytest
 from packages.core import types
+from tb_device_mqtt import TBDeviceMqttClient  # type: ignore
+import time
 
 
 @pytest.mark.integration
-def test_uploading() -> None:
+def test_thingsboard_connection() -> None:
     config = types.Config.load()
-    if config.upload is None:
+    if config.thingsboard is None:
         return
 
-    raise Exception("Could not establish a connection to ThingsBoard.")
+    # initialize MQTT client
+    client = TBDeviceMqttClient(
+        config.thingsboard.host, username=config.thingsboard.access_token
+    )
+
+    client.connect()
+    time.sleep(2)
+    assert client.is_connected()
+    client.disconnect()
