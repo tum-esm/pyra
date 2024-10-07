dd998b0113dfca1aeb9c3292e86ffcb511f34d2d
Moritz Makowski <moritz.makowski@tum.de>
2024-10-07 11:21:22 +0200

    Move system checks into thread (1)
    Add new thread class

---
+++ packages/core/threads/system_checks_thread.py
@@ -0,0 +1,77 @@
+import threading
+import tum_esm_utils
+from .abstract_thread import AbstractThread
+from packages.core import interfaces, types, utils
+
+
+class SystemChecksThread(AbstractThread):
+    """Thread for checking the system's state (CPU usage, disk utilization, etc.)"""
+    @staticmethod
+    class StorageError(Exception):
+        """Raised when storage is more than 90% full"""
+
+    @staticmethod
+    class LowEnergyError(Exception):
+        """Raised when battery is less than 20% full"""
+
+    @staticmethod
+    def should_be_running(config: types.Config) -> bool:
+        """Based on the config, should the thread be running or not?"""
+
+        return True
+
+    @staticmethod
+    def get_new_thread_object() -> threading.Thread:
+        """Return a new thread object that is to be started."""
+        return threading.Thread(target=SystemChecksThread.main, daemon=True)
+
+    @staticmethod
+    def main(headless: bool = False) -> None:
+        """Main entrypoint of the thread. In headless mode, 
+        don't write to log files but print to console."""
+
+        logger = utils.Logger(origin="system-checks", just_print=headless)
+        logger.info("Running SystemChecks")
+
+        while True:
+
+            cpu_usage = tum_esm_utils.system.get_cpu_usage()
+            logger.debug(f"Current CPU usage for all cores is {cpu_usage}%.")
+
+            memory_usage = tum_esm_utils.system.get_memory_usage()
+            logger.debug(
+                f"Current v_memory usage for the system is {memory_usage}."
+            )
+
+            last_boot_time = tum_esm_utils.system.get_last_boot_time()
+            logger.debug(f"The system is running since {last_boot_time}.")
+
+            # DISK SPACE
+
+            disk_space = tum_esm_utils.system.get_disk_space()
+            logger.debug(f"The disk is currently filled with {disk_space}%.")
+            if disk_space > 90:
+                raise SystemChecksThread.StorageError(
+                    "Disk space is less than 10%. This is bad for the OS stability."
+                )
+
+            # BATTERY LEVEL
+
+            battery_level = tum_esm_utils.system.get_system_battery()
+            logger.debug(f"The battery level is {battery_level}%.")
+            if battery_level is not None:
+                if battery_level < 20:
+                    raise SystemChecksThread.LowEnergyError(
+                        "The battery of the system is below 20%. Please check the power supply."
+                    )
+
+            interfaces.StateInterface.update_state(
+                operating_system_state=types.OperatingSystemState(
+                    cpu_usage=cpu_usage,
+                    memory_usage=memory_usage,
+                    last_boot_time=str(last_boot_time),
+                    filled_disk_space_fraction=disk_space,
+                )
+            )
+
+            logger.info("Waiting 3 minutes before next system check")
