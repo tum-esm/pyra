4e06f2a729991c2b7267ce83b66466c5451b2af9
Moritz Makowski <moritz.makowski@tum.de>
2024-10-07 18:26:58 +0200

    Write new OPUS Controller (2)
    Add class to handle Opus program state

--- packages/core/threads/opus_control_thread.py
+++ packages/core/threads/opus_control_thread.py
@@ -68,3 +68,87 @@ class DDEConnection:
         assert self.conversation is not None
         return self.conversation.Request(command)
 
+
+class OpusProgram:
+    """TODO: docstring"""
+    @staticmethod
+    def start(config: types.Config) -> None:
+        """Starts the OPUS.exe with os.startfile()."""
+
+        interfaces.ActivityHistoryInterface.add_datapoint(opus_startups=1)
+
+        # works only >= python3.10 because it requires the "cwd" argument
+        assert sys.platform == "win32", f"this function cannot be run on platform {sys.platform}"
+        assert sys.version_info.major >= 3 and sys.version_info.minor >= 10, "this function requires python >= 3.10"
+
+        os.startfile(  # type: ignore
+            os.path.basename(config.opus.executable_path.root),
+            cwd=os.path.dirname(config.opus.executable_path.root),
+            arguments=f"/LANGUAGE=ENGLISH /DIRECTLOGINPASSWORD={self.config.opus.username}@{self.config.opus.password}",
+            show_cmd=2,
+        )
+        tum_esm_utils.timing.wait_for_condition(
+            is_successful=OpusProgram.is_running,
+            timeout_message="OPUS.exe did not start within 90 seconds.",
+            timeout_seconds=90,
+            check_interval_seconds=8,
+        )
+
+    @staticmethod
+    def is_running() -> bool:
+        """Checks if OPUS is already running by searching for processes with
+        the executable `opus.exe` or `OpusCore.exe`
+
+        Returns: `True` if Application is currently running and `False` if not."""
+
+        for p in psutil.process_iter():
+            try:
+                if p.name() in ["opus.exe", "OpusCore.exe"]:
+                    return True
+            except (
+                psutil.AccessDenied,
+                psutil.ZombieProcess,
+                psutil.NoSuchProcess,
+                IndexError,
+            ):
+                pass
+
+        return False
+
+    @staticmethod
+    def stop(dde_connection: Optional[DDEConnection] = None) -> None:
+        """Closes OPUS via DDE/force kills it via psutil. If no DDEConnection
+        is provided, the function will force kill OPUS right away."""
+
+        if (dde_connection is not None) and dde_connection.is_working():
+            assert sys.platform == "win32", f"this function cannot be run on platform {sys.platform}"
+
+            logger.info(f"Requesting to stop OPUS via DDE")
+            dde_connection.request("CLOSE_OPUS")
+
+            logger.info(f"Waiting for OPUS to close gracefully")
+            try:
+                tum_esm_utils.timing.wait_for_condition(
+                    is_successful=lambda: not OpusMeasurement.opus_is_running(),
+                    timeout_message="OPUS.exe did not stop within 60 seconds.",
+                    timeout_seconds=60,
+                    check_interval_seconds=4,
+                )
+                logger.info("OPUS.exe stopped successfully.")
+                return
+            except TimeoutError:
+                logger.warning("OPUS.exe did not stop gracefully within 60 seconds.")
+
+        for p in psutil.process_iter():
+            try:
+                if p.name() in ["opus.exe", "OpusCore.exe"]:
+                    p.kill()
+            except (
+                psutil.AccessDenied,
+                psutil.ZombieProcess,
+                psutil.NoSuchProcess,
+                IndexError,
+            ):
+                pass
+
+
