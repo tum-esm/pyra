cd08634ab7e98ff0e815a4f4ba9d8855dbd336e1
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-10-10 13:25:23 +0200

    Write new CamTracker Controller (2)
    Write logic to start and stop CamTracker

--- packages/core/threads/camtracker_thread.py
+++ packages/core/threads/camtracker_thread.py
@@ -8,6 +8,95 @@ from packages.core import interfaces, types, utils
 logger = utils.Logger(origin="camtracker")
 
 
+class CamTrackerProgram:
+    @staticmethod
+    def start(config: types.Config) -> None:
+        """Starts the OPUS.exe with os.startfile()."""
+
+        interfaces.ActivityHistoryInterface.add_datapoint(camtracker_startups=1)
+
+        # removing old stop file
+        stop_file_path = os.path.join(
+            os.path.dirname(config.camtracker.executable_path.root), "stop.txt"
+        )
+        if os.path.exists(stop_file_path):
+            os.remove(stop_file_path)
+
+        logger.info("Starting CamTracker")
+        os.startfile(  # type: ignore
+            os.path.basename(config.camtracker.executable_path.root),
+            cwd=os.path.dirname(config.camtracker.executable_path.root),
+            arguments="-autostart",
+            show_cmd=2,
+        )
+        tum_esm_utils.timing.wait_for_condition(
+            is_successful=CamTrackerProgram.is_running,
+            timeout_message="CamTracker did not start within 90 seconds.",
+            timeout_seconds=90,
+            check_interval_seconds=8,
+        )
+        logger.info("Successfully started CamTracker")
+
+    @staticmethod
+    def is_running() -> bool:
+        """Checks if CamTracker is already running by searching for processes with
+        the executable `camtracker*.exe`."""
+
+        logger.debug("Checking if CamTracker is running")
+        for p in psutil.process_iter():
+            try:
+                name = p.name().lower()
+                if name.startswith("camtracker") and name.endswith(".exe"):
+                    return True
+            except (
+                psutil.AccessDenied,
+                psutil.ZombieProcess,
+                psutil.NoSuchProcess,
+                IndexError,
+            ):
+                pass
+
+        return False
+
+    @staticmethod
+    def stop(config: types.Config) -> None:
+        """Closes OPUS via DDE/force kills it via psutil. If no DDEConnection
+        is provided, the function will force kill OPUS right away."""
+
+        stop_file_path = os.path.join(
+            os.path.dirname(config.camtracker.executable_path.root), "stop.txt"
+        )
+        logger.info("Trying to stop CamTracker gracefully")
+        with open(stop_file_path, "w") as f:
+            f.write("")
+
+        try:
+            tum_esm_utils.timing.wait_for_condition(
+                is_successful=lambda: not CamTrackerProgram.is_running(),
+                timeout_seconds=90,
+                check_interval_seconds=9,
+            )
+            logger.info("Successfully stopped CamTracker")
+            return
+        except TimeoutError as e:
+            logger.error("Camtracker did not stop within 90 seconds.")
+
+        logger.info("Force killing CamTracker")
+        for p in psutil.process_iter():
+            try:
+                name = p.name().lower()
+                if name.startswith("camtracker") and name.endswith(".exe"):
+                    p.kill()
+            except (
+                psutil.AccessDenied,
+                psutil.ZombieProcess,
+                psutil.NoSuchProcess,
+                IndexError,
+            ):
+                pass
+        logger.info("Successfully force killed CamTracker")
+
+
 class CamTrackerThread(AbstractThread):
     @staticmethod
     def should_be_running(config: types.Config) -> bool:
