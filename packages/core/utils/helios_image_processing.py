5ffd1d25f112c5522e606b8a39833659a73be4e1
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-12-10 02:16:33 +0100

    #236 Improve Helios (6)
    Refine new image processing

--- packages/core/utils/helios_image_processing.py
+++ packages/core/utils/helios_image_processing.py
@@ -35,7 +35,7 @@ class HeliosImageProcessing:
             - (np.sum(image * outer_mask) / np.sum(outer_mask))
         )
 
-    def _get_lense_position(bw_image: np.ndarray) -> Optional[tuple[float, float, float]]:
+    def get_lense_position(bw_image: np.ndarray) -> Optional[tuple[float, float, float]]:
         """Determine the position of the lense in the image."""
 
         bw_image = bw_image * (60 / np.mean(bw_image))
@@ -79,7 +79,7 @@ class HeliosImageProcessing:
         sorted_ranking = sorted(ranking, key=lambda x: x[0] + x[1])
         best_circle_index = sorted_ranking[0][1]
 
-        return cx[best_circle_index], cy[best_circle_index], radii[best_circle_index]
+        return cx[best_circle_index] * 2, cy[best_circle_index] * 2, radii[best_circle_index] * 2
 
     @staticmethod
     def _annotate_processed_image(
@@ -88,30 +88,31 @@ class HeliosImageProcessing:
         circle_cx: int,
         circle_cy: int,
         circle_r: int,
-    ) -> Image:
+    ) -> Image.Image:
         """Put text for edge fraction and mark circles in image."""
-        rgb_frame = np.stack((bw_frame,) * 3, axis=-1)
+        rgb_frame = skimage.color.gray2rgb(bw_frame)
 
         # draw outer circle
-        rr, cc = skimage.draw.disk((circle_cy, circle_cx), circle_r, shape=bw_frame.shape)
-        outer_circle = np.zeros(rgb_frame.shape, dtype=np.uint8)
-        outer_circle[rr, cc, 0] = 255
-        outer_circle = skimage.morphology.dilation(outer_circle, skimage.morphology.disk(3))
+        rr, cc = skimage.draw.circle_perimeter(circle_cy, circle_cx, circle_r, shape=bw_frame.shape)
+        outer_circle = np.zeros(bw_frame.shape, dtype=np.uint8)
+        outer_circle[rr, cc] = 1
+        outer_circle = skimage.morphology.dilation(outer_circle, skimage.morphology.disk(2))
 
         # draw inner circle
-        rr, cc = skimage.draw.disk(
-            (circle_cy, circle_cx), round(circle_r * 0.9), shape=bw_frame.shape
+        rr, cc = skimage.draw.circle_perimeter(
+            circle_cy, circle_cx, round(circle_r * 0.9), shape=bw_frame.shape
         )
-        inner_circle = np.zeros(rgb_frame.shape, dtype=np.uint8)
-        inner_circle[rr, cc, 2] = 255
-        inner_circle = skimage.morphology.dilation(inner_circle, skimage.morphology.disk(3))
+        inner_circle = np.zeros(bw_frame.shape, dtype=np.uint8)
+        inner_circle[rr, cc] = 1
+        inner_circle = skimage.morphology.dilation(inner_circle, skimage.morphology.disk(2))
 
         # combine images
-        img = rgb_frame + outer_circle + inner_circle
-        img[img > 255] = 255
+        rgb_frame[:, :, 0] = rgb_frame[:, :, 0] + outer_circle
+        rgb_frame[:, :, 2] = rgb_frame[:, :, 2] + inner_circle
+        rgb_frame[rgb_frame > 1] = 1
 
         # add text
-        pil_image = Image.fromarray(img)
+        pil_image = Image.fromarray((rgb_frame * 255).astype(np.uint8))
         draw = ImageDraw.Draw(pil_image)
         draw.text((10, 10), f"{edge_fraction * 100:.2f}%", (255, 255, 255))
         draw.text((10, 30), f"{datetime.datetime.now()}", (255, 255, 255))
@@ -129,7 +130,7 @@ class HeliosImageProcessing:
 
     @staticmethod
     def get_edge_fraction(
-        frame: np.ndarray[Any, Any],
+        rgb_frame: np.ndarray[Any, Any],
         station_id: str,
         edge_color_threshold: int,
         target_pixel_brightness: float,
@@ -142,41 +143,42 @@ class HeliosImageProcessing:
         The status is 1 when the edge pixels are above the given threshold
         and 0 otherwise."""
 
+        # convert the image to black and white
+        bw_frame: np.ndarray = skimage.color.rgb2gray(rgb_frame)
+
         # adjust the brightness of the frame to a target value
-        bw_frame: np.ndarray = np.mean(frame, axis=2)
-        evenly_lit_frame: np.ndarray = bw_frame * (target_pixel_brightness / np.mean(frame))
+        evenly_lit_frame: np.ndarray = bw_frame * (target_pixel_brightness / np.mean(bw_frame))
 
         # transform image from 1280x720 to 640x360
-        downscaled_image: np.ndarray = skimage.transform.rescale(evenly_lit_frame, 0.5)
+        # downscaled_image: np.ndarray = skimage.transform.rescale(evenly_lit_frame, 0.5)
 
         # only consider edges and make them bold
         edges_dilated: np.ndarray = skimage.morphology.dilation(
             skimage.feature.canny(
-                downscaled_image,
+                evenly_lit_frame,
                 sigma=7,
-                low_threshold=round(edge_color_threshold / 3),
+                low_threshold=round(edge_color_threshold * 0.8),
                 high_threshold=edge_color_threshold,
             ),
-            skimage.morphology.disk(5),
-        )
+            skimage.morphology.disk(2),
+        ).astype(np.uint8)
 
         # blacken the outer 10% of the circle radius
         inner_mask = np.zeros(evenly_lit_frame.shape, dtype=np.uint8)
-        inner_mask[
-            skimage.draw.disk(
-                center=(lense_circle[1], lense_circle[0]),
-                radius=lense_circle[2] * 0.9,
-                shape=evenly_lit_frame.shape,
-            )
-        ] = 1
-        edges_dilated_masked: np.ndarray = edges_dilated * inner_mask
+        cc, rr = skimage.draw.disk(
+            center=(lense_circle[1], lense_circle[0]),
+            radius=lense_circle[2] * 0.9,
+            shape=evenly_lit_frame.shape,
+        )
+        inner_mask[cc, rr] = 1
+        edges_dilated = edges_dilated * inner_mask
 
         # determine how many pixels inside the circle are made up of "edge pixels"
         pixels_inside_circle: int = np.sum(3.141592 * pow(lense_circle[2] * 0.9, 2))
         edge_fraction: float = 0
         if pixels_inside_circle != 0:
             edge_fraction = round(
-                (np.sum(edges_dilated_masked) / 255)  # type: ignore
+                (np.sum(edges_dilated))  # type: ignore
                 / pixels_inside_circle,
                 6,
             )
@@ -192,25 +194,30 @@ class HeliosImageProcessing:
             processed_frame = HeliosImageProcessing._annotate_processed_image(
                 edges_dilated, edge_fraction, *lense_circle
             )
-            if save_images_to_archive:
-                skimage.io.imsave(
-                    os.path.join(
-                        img_directory_path,
-                        f"{station_id}-{img_timestamp}-{edge_fraction_str}-raw.jpg",
-                    ),
-                    frame,
+
+            if save_images_to_archive or save_current_image:
+                raw_image = Image.fromarray(
+                    (skimage.color.gray2rgb(bw_frame) * 255).astype(np.uint8)
                 )
-                skimage.io.imsave(
-                    os.path.join(
-                        img_directory_path,
-                        f"{station_id}-{img_timestamp}-{edge_fraction_str}-processed.jpg",
-                    ),
-                    processed_frame,
-                )
-            if save_current_image:
-                skimage.io.imsave(os.path.join(_LOGS_DIR, "current-helios-view-raw.jpg"), frame)
-                skimage.io.imsave(
-                    os.path.join(_LOGS_DIR, "current-helios-view-processed.jpg"), processed_frame
-                )
+                if save_images_to_archive:
+                    raw_image.save(
+                        os.path.join(
+                            img_directory_path,
+                            f"{station_id}-{img_timestamp}-{edge_fraction_str}-raw.jpg",
+                        ),
+                    )
+                    processed_frame.save(
+                        os.path.join(
+                            img_directory_path,
+                            f"{station_id}-{img_timestamp}-{edge_fraction_str}-edges.jpg",
+                        ),
+                    )
+                if save_current_image:
+                    raw_image.save(
+                        os.path.join(_LOGS_DIR, "current-helios-view-raw.jpg"),
+                    )
+                    processed_frame.save(
+                        os.path.join(_LOGS_DIR, "current-helios-view-processed.jpg"),
+                    )
 
         return edge_fraction
