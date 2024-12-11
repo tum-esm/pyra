65004e09839fe4adfaf5722d5487730c1fd71661
Moritz Makowski <moritz.makowski@tum.de>
2024-12-11 17:18:06 +0100

    #235 Set peak position automatically (10)
    Add timeout limit to EM27 requests

--- packages/core/interfaces/em27_interface.py
+++ packages/core/interfaces/em27_interface.py
@@ -12,14 +12,12 @@ class EM27Interface:
         """Get the peak position of the EM27.
 
         This reads the ABP value from the EM27 via http://{ip}/config/servmenuA.htm"""
-        body = (
-            requests.get(f"http://{ip.root}/config/servmenuA.htm")
-            .text.replace("\n", "")
-            .replace("\t", "")
-            .replace(" ", "")
-            .lower()
-        )
-        r = re.findall(r'<inputname="abp"value="(\d+)"', body)
+        try:
+            raw_body = requests.get(f"http://{ip.root}/config/servmenuA.htm", timeout=3)
+        except:
+            return None
+        body = raw_body.text.replace("\n", "").replace("\t", "").replace(" ", "").lower()
+        r: list[str] = re.findall(r'<inputname="abp"value="(\d+)"', body)
         if len(r) != 1:
             return None
         return int(r[0])
@@ -32,4 +30,11 @@ class EM27Interface:
         """Set the peak position of the EM27.
 
         It is equivalent to setting the ABP via http://{ip}/config/servmenuA.htm"""
-        requests.get(f"http://{ip.root}/config/servmenuA.htm?sub=Send^&ABP={new_peak_position}^")
+        try:
+            requests.get(
+                f"http://{ip.root}/config/servmenuA.htm?sub=Send^&ABP={new_peak_position}^",
+                timeout=3,
+            )
+        except Exception as e:
+            raise RuntimeError(f"Could not set peak position: {e}")
+
