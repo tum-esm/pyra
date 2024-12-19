3dafa5ab89d2a0e128c8e3c3556728b1c9cf7abc
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-12-19 23:44:50 +0100

    Upgrade from Tauri v1 to v2 (1)
    Migrate dependencies and setup

--- packages/ui/src-tauri/src/main.rs
+++ packages/ui/src-tauri/src/main.rs
@@ -1,10 +1,13 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
