48f96c36ac60bfb553562b02ccd2ac07fb1dec26
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-12-25 12:19:34 +0100

    Update Activity History for the multithreading age (12)
    update parsing of new schema in UI
    make it more performant by not having 15 minute bins, but merged bins

--- packages/ui/src/utils/zustand-utils/activity-zustand.ts
+++ packages/ui/src/utils/zustand-utils/activity-zustand.ts
@@ -2,72 +2,84 @@ import { create } from 'zustand';
 import { z } from 'zod';
 import { groupBy, sumBy } from 'lodash';
 
-export const ACTIVITY_BUCKETS_PER_HOUR = 4;
-
-const activityHistorySchema = z.array(
-    z
-        .object({
-            local_time: z.string(),
-            is_measuring: z.boolean(),
-            has_errors: z.boolean(),
-            is_uploading: z.boolean(),
-            camtracker_startups: z.number(),
-            opus_startups: z.number(),
-            cli_calls: z.number(),
-        })
-        .transform((data) => ({
-            localTime: data.local_time,
-            isMeasuring: data.is_measuring,
-            hasErrors: data.has_errors,
-            isUploading: data.is_uploading,
-            camtrackerStartups: data.camtracker_startups,
-            opusStartups: data.opus_startups,
-            cliCalls: data.cli_calls,
-        }))
-);
-
+const activityHistorySchema = z.object({
+    core_is_running: z.array(z.number()).length(24 * 60),
+    is_measuring: z.array(z.number()).length(24 * 60),
+    has_errors: z.array(z.number()).length(24 * 60),
+    camtracker_startups: z.array(z.number()).length(24 * 60),
+    opus_startups: z.array(z.number()).length(24 * 60),
+    cli_calls: z.array(z.number()).length(24 * 60),
+    upload_is_running: z.array(z.number()).length(24 * 60),
+});
 export type ActivityHistory = z.infer<typeof activityHistorySchema>;
 
 export type ActivitySection = {
-    from_hour: number;
-    to_hour: number;
-    isRunning: number;
-    isMeasuring: number;
-    hasErrors: number;
-    isUploading: number;
-    camtrackerStartups: number;
-    opusStartups: number;
-    cliCalls: number;
+    from_minute_index: number;
+    to_minute_index: number;
 };
 
-function parseActivityHistory(activityHistory: ActivityHistory): ActivitySection[] {
-    const groupedBySection = groupBy(activityHistory, (ah) => {
-        const hour = parseInt(ah.localTime.split(':')[0]);
-        const minute = parseInt(ah.localTime.split(':')[1]);
-        return (
-            hour + Math.floor((minute / 60) * ACTIVITY_BUCKETS_PER_HOUR) / ACTIVITY_BUCKETS_PER_HOUR
-        );
-    });
-    return Object.entries(groupedBySection).map(([key, value]) => ({
-        from_hour: parseFloat(key),
-        to_hour: parseFloat(key) + 1 / ACTIVITY_BUCKETS_PER_HOUR,
-        isRunning: value.length,
-        isMeasuring: sumBy(value, 'isMeasuring'),
-        hasErrors: sumBy(value, 'hasErrors'),
-        isUploading: sumBy(value, 'isUploading'),
-        camtrackerStartups: sumBy(value, 'camtrackerStartups'),
-        opusStartups: sumBy(value, 'opusStartups'),
-        cliCalls: sumBy(value, 'cliCalls'),
-    }));
+function parseActivityHistoryTimeSeries(ts: number[]): ActivitySection[] {
+    const sections: ActivitySection[] = [];
+    let currentSection: ActivitySection | undefined = undefined;
+    for (let i = 0; i < ts.length; i++) {
+        if (ts[i] > 0) {
+            if (!currentSection) {
+                currentSection = {
+                    from_minute_index: i,
+                    to_minute_index: i,
+                };
+            } else {
+                currentSection.to_minute_index = i;
+            }
+        } else {
+            if (currentSection) {
+                sections.push(currentSection);
+                currentSection = undefined;
+            }
+        }
+    }
+    if (currentSection) {
+        sections.push(currentSection);
+    }
+    return sections;
 }
 
 interface ActivityHistoryStore {
-    activitySections: ActivitySection[] | undefined;
+    activitySections: {
+        core_is_running: ActivitySection[];
+        is_measuring: ActivitySection[];
+        has_errors: ActivitySection[];
+        camtracker_startups: ActivitySection[];
+        opus_startups: ActivitySection[];
+        cli_calls: ActivitySection[];
+        upload_is_running: ActivitySection[];
+    };
+
     setActivityHistory: (ah: any) => void;
 }
 
 export const useActivityHistoryStore = create<ActivityHistoryStore>()((set) => ({
-    activitySections: undefined,
-    setActivityHistory: (ah: any) =>
-        set(() => ({ activitySections: parseActivityHistory(activityHistorySchema.parse(ah)) })),
+    activitySections: {
+        core_is_running: [],
+        is_measuring: [],
+        has_errors: [],
+        camtracker_startups: [],
+        opus_startups: [],
+        cli_calls: [],
+        upload_is_running: [],
+    },
+    setActivityHistory: (ah: string) => {
+        const parsed = activityHistorySchema.parse(ah);
+        set(() => ({
+            activitySections: {
+                core_is_running: parseActivityHistoryTimeSeries(parsed.core_is_running),
+                is_measuring: parseActivityHistoryTimeSeries(parsed.is_measuring),
+                has_errors: parseActivityHistoryTimeSeries(parsed.has_errors),
+                camtracker_startups: parseActivityHistoryTimeSeries(parsed.camtracker_startups),
+                opus_startups: parseActivityHistoryTimeSeries(parsed.opus_startups),
+                cli_calls: parseActivityHistoryTimeSeries(parsed.cli_calls),
+                upload_is_running: parseActivityHistoryTimeSeries(parsed.upload_is_running),
+            },
+        }));
+    },
 }));
