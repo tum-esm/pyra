9430e6adb5964dc927253c6755fc953266368a7a
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-01-13 21:52:57 +0100

    #238 Solve OPUS communication issues (14)
    Remove unused copy of OPUS HTTP interface

--- packages/core/interfaces/__init__.py
+++ packages/core/interfaces/__init__.py
@@ -1,5 +1,4 @@
 from .activity_history import ActivityHistoryInterface as ActivityHistoryInterface
 from .em27_interface import EM27Interface as EM27Interface
-from .opus_interface import OPUSHTTPInterface as OPUSHTTPInterface
 from .state_interface import StateInterface as StateInterface
 from .tum_enclosure_interface import TUMEnclosureInterface as TUMEnclosureInterface
