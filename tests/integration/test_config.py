2bde15fa5b761f300a9d5bf67116b189742e2812
Moritz Makowski <moritz.makowski@tum.de>
2025-02-20 16:51:30 +0100

    Improve integration tests (1)
    test `config.json`

---
+++ tests/integration/test_config.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pytest
+import tum_esm_utils
+
+PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=3)
+CONFIG_DIR = os.path.join(PROJECT_DIR, "config")
+
+sys.path.append(PROJECT_DIR)
+from packages.core import types
+
+
+@pytest.mark.integration
+def test_config() -> None:
+    types.Config.load()
