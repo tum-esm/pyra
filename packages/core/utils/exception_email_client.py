dc243e18ef34f4fc0d4f8176e0792135ddb69abc
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-03-17 13:57:32 +0100

    #244 Crop Error Email logs correctly

--- packages/core/utils/exception_email_client.py
+++ packages/core/utils/exception_email_client.py
@@ -34,7 +34,7 @@ def _get_current_log_lines() -> list[str]:
 
     log_lines_in_email: list[str] = []
     included_iterations = 0
-    for l in latest_log_lines[::-1][:50]:
+    for l in latest_log_lines[::-1][:200]:
         if ("main - INFO - Starting iteration" in l) or ("main - INFO - Starting mainloop" in l):
             included_iterations += 1
         if 'running command "config update" with content:' in l:
