9c6b37c28b8968f09d024e3ec95b2404d323ab99
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-07-24 15:46:39 +0200

    Add COCCON Spain Enclosure code frame (2)
    Add enclosure types

--- packages/core/types/enclosures/__init__.py
+++ packages/core/types/enclosures/__init__.py
@@ -1,1 +1,2 @@
 from . import tum_enclosure as tum_enclosure
+from . import coccon_spain_enclosure as coccon_spain_enclosure
---
+++ packages/core/types/enclosures/coccon_spain_enclosure.py
@@ -0,0 +1,41 @@
+from typing import Optional
+import datetime
+
+from tum_esm_utils.validators import StricterBaseModel, StrictIPv4Adress
+
+# --- CONFIG ---
+
+# TODO: add the config options you need
+
+
+class COCCONSpainEnclosureConfig(StricterBaseModel):
+    ip: StrictIPv4Adress
+
+
+class PartialCOCCONSpainEnclosureConfig(StricterBaseModel):
+    """Like `COCCONSpainEnclosureConfig`, but all fields are optional."""
+
+    ip: Optional[StrictIPv4Adress] = None
+
+
+# --- STATE ---
+
+# TODO: add the state variables you need
+
+
+class ActorsState(StricterBaseModel):
+    fan_speed: Optional[int] = None
+    cover_position: Optional[int] = None
+
+
+class SensorsState(StricterBaseModel):
+    humidity: Optional[float] = None
+    temperature: Optional[float] = None
+    wind_direction: Optional[float] = None
+    wind_speed: Optional[float] = None
+
+
+class COCCONSpainEnclosureState(StricterBaseModel):
+    last_full_fetch: Optional[datetime.datetime] = None
+    actors: ActorsState = ActorsState()
+    sensors: SensorsState = SensorsState()
