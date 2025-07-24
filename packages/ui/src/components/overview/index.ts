5b06fe199c90cd99eeb39b94ff22546195283392
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-07-24 16:27:57 +0200

    Remove unused UI Code

--- packages/ui/src/components/overview/index.ts
+++ packages/ui/src/components/overview/index.ts
@@ -1,12 +1,11 @@
 import ActivityPlot from './activity-plot';
 import MeasurementDecision from './measurement-decision';
 import PyraCoreStatus from './pyra-core-status';
-import { SystemState, TumEnclosureState } from './system-state';
+import { SystemState } from './system-state';
 
 export default {
     ActivityPlot,
     PyraCoreStatus,
     MeasurementDecision,
     SystemState,
-    TumEnclosureState,
 };
