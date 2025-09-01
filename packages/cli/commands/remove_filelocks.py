eed7ab04ebcbf82cc95e60d565d1a70032318702
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-09-01 11:00:34 +0200

    #266 Migrate state semaphore from file lock to thread lock (10)
    Deprecate CLI function "remove-filelocks"

--- packages/cli/commands/remove_filelocks.py
+++ packages/cli/commands/remove_filelocks.py
@@ -1,36 +1,16 @@
-"""Remove all filelocks. Helpful when any of the programs crashed during
-writing to a file. Normally, this should not be necessary."""
-
-import os
+"""Not doing anything because Pyra >= 4.2.4 is no longer using file locks."""
 
 import click
-
-from packages.core import interfaces, utils
-
-_dir = os.path.dirname
-_PROJECT_DIR = _dir(_dir(_dir(_dir(os.path.abspath(__file__)))))
-logger = utils.Logger(origin="cli", lock=None)
 
 
 def _print_green(text: str) -> None:
     click.echo(click.style(text, fg="green"))
 
 
+# FIXME: remove with next breaking release
 @click.command(
     name="remove-filelocks",
-    help="Remove all filelocks. Helpful when any of the programs crashed during writing to a file. Normally, this should not be necessary.",
+    help="Not doing anything because Pyra >= 4.2.4 is no longer using file locks.",
 )
 def remove_filelocks() -> None:
-    with interfaces.StateInterface.update_state(logger) as s:
-        s.activity.cli_calls += 1
-    logger.debug('running command "remove-filelocks"')
-    lock_files = [
-        os.path.join(_PROJECT_DIR, "config", ".config.lock"),
-        os.path.join(_PROJECT_DIR, "config", ".state.lock"),
-        os.path.join(_PROJECT_DIR, "logs", ".logs.lock"),
-    ]
-    for f in lock_files:
-        if os.path.isfile(f):
-            os.remove(f)
-            _print_green(f"Removing {f}")
-    _print_green("Done!")
+    _print_green("Not doing anything because Pyra >= 4.2.4 is no longer using file locks.")
