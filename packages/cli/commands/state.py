54fb1d819fa5f962caaca5e702a18105fb6797c8
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-09-01 11:12:41 +0200

    #266 Migrate state semaphore from file lock to thread lock (13)
    Write new logic how the CLI reads the state file

--- packages/cli/commands/state.py
+++ packages/cli/commands/state.py
@@ -1,10 +1,19 @@
 """Read the current state.json file."""
 
+import datetime
+import os
+import threading
+import time
 import click
+import tum_esm_utils
 
-from packages.core import interfaces, utils
+from packages.core import utils, types
 
 logger = utils.Logger(origin="cli", lock=None)
+state_lock = threading.Lock()
+
+_PROJECT_DIR = tum_esm_utils.files.rel_to_abs_path("../../..")
+_STATE_FILE_PATH = os.path.join(_PROJECT_DIR, "logs", "state.json")
 
 
 @click.group()
@@ -16,5 +25,29 @@ def state_command_group() -> None:
 @click.option("--indent", is_flag=True, help="Print the JSON in an indented manner")
 def _get_state(indent: bool) -> None:
     logger.debug('running command "state get"')
-    state = interfaces.StateInterface.load_state(logger)
+    state: types.StateObject
+
+    # if state file does not exist, create it
+    if not os.path.exists(_STATE_FILE_PATH):
+        logger.warning("State file does not exist - Creating new one.")
+        state = types.StateObject(last_updated=datetime.datetime.now())
+        tum_esm_utils.files.dump_file(_STATE_FILE_PATH, state.model_dump_json(indent=4))
+
+    # otherwise try to load it every 0.2 seconds until it is readable
+    else:
+        start_time = time.time()
+        while True:
+            f = tum_esm_utils.files.load_file(_STATE_FILE_PATH)
+            try:
+                state = types.StateObject.model_validate_json(f)
+                break
+            except Exception as e:
+                time.sleep(0.2)
+
+            if (time.time() - start_time) > 60:
+                logger.warning("Could not read state file after 60 seconds - Creating new one.")
+                state = types.StateObject(last_updated=datetime.datetime.now())
+                tum_esm_utils.files.dump_file(_STATE_FILE_PATH, state.model_dump_json(indent=4))
+                break
+
     click.echo(state.model_dump_json(indent=(2 if indent else None)))
