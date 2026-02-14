38c056e79d86022fc3bb69a02ebea493055da123
Moritz Makowski <moritz.makowski@protonmail.com>
2026-02-14 16:52:48 +0000

    #276 Integrate COCCON Spain Enclosure (5)
    Add actually used state

--- packages/core/types/enclosures/aemet.py
+++ packages/core/types/enclosures/aemet.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Literal, Optional
 import datetime
 
+import pydantic
 from tum_esm_utils.validators import StricterBaseModel, StrictIPv4Adress
 
 # --- CONFIG ---
@@ -20,22 +21,173 @@ class PartialAEMETEnclosureConfig(StricterBaseModel):
 
 # --- STATE ---
 
-# TODO: add the state variables you need
 
+class AEMETEnclosureState(pydantic.BaseModel):
+    # general
+    time: Optional[datetime.datetime] = pydantic.Field(
+        default=None,
+        description="Time of this datalogger state.",
+    )
+    battery_voltage: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="BattV",
+        description="Battery voltage in volts.",
+    )
+    logger_panel_temperature: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="PTemp_C",
+        description="Temperature of the logger panel in degrees celsius.",
+    )
+    auto_mode: Optional[Literal[0, 1]] = pydantic.Field(
+        default=None,
+        validation_alias="AUTO_",
+        description="Enclosure mode. auto if the enclosure is in automatic mode, manual if it is in manual mode.",
+    )
+    enhanced_security_mode: Optional[Literal[0, 1]] = pydantic.Field(
+        default=None,
+        validation_alias="ENHANCED_SECURITY",
+        description="Whether the enhanced security mode is active. 0 if not, 1 if it is active.",
+    )
 
-class ActorsState(StricterBaseModel):
-    fan_speed: Optional[float] = None
-    cover_position: Optional[float] = None
+    # weather sensor readings
+    air_pressure_internal: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="Press_Int",
+        description="Internal pressure in hPa.",
+    )
+    air_pressure_external: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="Press_Ext",
+        description="External pressure in hPa.",
+    )
+    relative_humidity_internal: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="RH_Int",
+        description="Internal relative humidity in percentage.",
+    )
+    relative_humidity_external: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="RH_Ext",
+        description="External relative humidity in percentage.",
+    )
+    air_temperature_internal: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="AirTC_Int",
+        description="Internal air temperature in degrees celsius.",
+    )
+    air_temperature_external: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="AirTC_Ext",
+        description="External air temperature in degrees celsius.",
+    )
+    dew_point_temperature_internal: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="Trocio_Int",
+        description="Internal dew point temperature in degrees celsius.",
+    )
+    dew_point_temperature_external: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="Trocio_Ext",
+        description="External dew point temperature in degrees celsius.",
+    )
+    wind_direction: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="Wdir",
+        description="Wind direction in degrees.",
+    )
+    wind_velocity: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="Wvel",
+        description="Wind velocity in m/s.",
+    )
+    rain_sensor_counter_1: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="LLUVIA_1_In",
+        description="Rain sensor counter 1. Negative values indicate no rain, positive values indicate rain. The higher the counter the more/longer it has been raining.",
+    )
+    rain_sensor_counter_2: Optional[float] = pydantic.Field(
+        default=None,
+        validation_alias="LLUVIA_2_In",
+        description="Rain sensor counter 2. Negative values indicate no rain, positive values indicate rain. The higher the counter the more/longer it has been raining.",
+    )
 
+    # closing reasons
+    closed_due_to_rain: Optional[bool] = pydantic.Field(
+        default=None,
+        validation_alias="Cerrar_Lluvia",
+        description="Whether the cover has been closed due to rain. 0 if not, 1 if it has been closed due to rain.",
+    )
+    closed_due_to_relative_humidity: Optional[bool] = pydantic.Field(
+        default=None,
+        validation_alias="Cerrar_RH_Ext",
+        description="Whether the cover has been closed due to high external relative humidity. 0 if not, 1 if it has been closed due to high external relative humidity.",
+    )
+    closed_due_to_external_air_temperature: Optional[bool] = pydantic.Field(
+        default=None,
+        validation_alias="Cerrar_AirTC_Ext",
+        description="Whether the cover has been closed due to high external air temperature. 0 if   not, 1 if it has been closed due to high external air temperature.",
+    )
+    closed_due_to_internal_relative_humidity: Optional[bool] = pydantic.Field(
+        default=None,
+        validation_alias="Cerrar_RH_Int",
+        description="Whether the cover has been closed due to high internal relative humidity. 0 if not, 1 if it has been closed due to high internal relative humidity.",
+    )
+    closed_due_to_internal_air_temperature: Optional[bool] = pydantic.Field(
+        default=None,
+        validation_alias="Cerrar_AirTC_Int",
+        description="Whether the cover has been closed due to high internal air temperature. 0 if not, 1 if it has been closed due to high internal air temperature.",
+    )
+    closed_due_to_wind_velocity: Optional[bool] = pydantic.Field(
+        default=None,
+        validation_alias="Cerrar_Wvel_Alta",
+        description="Whether the cover has been closed due to high wind velocity. 0 if not, 1 if it has been closed due to high wind velocity.",
+    )
 
-class SensorsState(StricterBaseModel):
-    humidity: Optional[float] = None
-    temperature: Optional[float] = None
-    wind_direction: Optional[float] = None
-    wind_speed: Optional[float] = None
+    # cover states
+    alert_level: Optional[Literal[0, 1, 2]] = pydantic.Field(
+        default=None,
+        validation_alias="ALERTA",
+        description="Alert level. 0 if no alert, 1 if counting towards closing, 2 if should be closed due to alert.",
+    )
+    aviera_fault_code: Optional[int] = pydantic.Field(
+        default=None,
+        validation_alias="AVERIA",
+        description="Value of AVERIA.",
+    )
+    cover_status: Optional[Literal["AF", "A.", "A", "C.", "CF", "C"]] = pydantic.Field(
+        default=None,
+        validation_alias="Estado_actual",
+        description="Current cover status. AF: opening (releasing fechillo), A.: opening, A: open, C.: closing, CF: closing (fechillo), C: closed.",
+    )
+    motor_position: Optional[int | str] = pydantic.Field(
+        default=None,
+        validation_alias="posicion",
+        description="Current motor position. 0 is fully open, higher values are more closed.",
+    )
 
-
-class AEMETEnclosureState(StricterBaseModel):
-    last_full_fetch: Optional[datetime.datetime] = None
-    actors: ActorsState = ActorsState()
-    sensors: SensorsState = SensorsState()
+    @property
+    def pretty_cover_status(
+        self,
+    ) -> Literal[
+        "opening-lock",
+        "opening-hood",
+        "open",
+        "closing-hood",
+        "closing-lock",
+        "closed",
+        "unknown",
+    ]:
+        if self.cover_status == "AF":
+            return "opening-lock"
+        elif self.cover_status == "A.":
+            return "opening-hood"
+        elif self.cover_status == "A":
+            return "open"
+        elif self.cover_status == "C.":
+            return "closing-hood"
+        elif self.cover_status == "CF":
+            return "closing-lock"
+        elif self.cover_status == "C":
+            return "closed"
+        else:
+            return "unknown"
