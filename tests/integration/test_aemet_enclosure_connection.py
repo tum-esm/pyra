8641bb53a65a078825bc4092e102fae5c2442ea7
Moritz Makowski <moritz.makowski@protonmail.com>
2026-02-15 16:42:41 +0000

    #276 Integrate COCCON Spain Enclosure (22)
    Add integration pytest to test connection to AEMET enclosure

---
+++ tests/integration/test_aemet_enclosure_connection.py
@@ -0,0 +1,25 @@
+import pytest
+import tum_esm_utils
+from packages.core import interfaces, types, utils
+
+
+@pytest.mark.order(3)
+@pytest.mark.integration
+def test_aemet_enclosure_connection() -> None:
+    config = types.Config.load()
+    logger = utils.Logger(origin="aemet-enclosure", lock=None, just_print=True)
+    state_lock = tum_esm_utils.sqlitelock.SQLiteLock(
+        filepath=interfaces.state_interface.STATE_LOCK_PATH,
+        timeout=interfaces.state_interface.STATE_LOCK_TIMEOUT,
+        poll_interval=interfaces.state_interface.STATE_LOCK_POLL_INTERVAL,
+    )
+
+    if config.aemet_enclosure is None:
+        pytest.skip("AEMET Enclosure is not configured")
+    else:
+        enclosure_interface = interfaces.AEMETEnclosureInterface(
+            config=config.aemet_enclosure,
+            state_lock=state_lock,
+            logger=logger,
+        )
+        enclosure_interface.read(immediate_write_to_central_state=False)
