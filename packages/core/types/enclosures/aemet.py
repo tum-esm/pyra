4846d4162f410b2b4347151071e711094eeec4e2
Moritz Makowski <moritz.makowski@protonmail.com>
2026-02-16 15:11:14 +0000

    #276 Integrate COCCON Spain Enclosure (41)
    Debug state parsing

--- packages/core/types/enclosures/aemet.py
+++ packages/core/types/enclosures/aemet.py
@@ -50,143 +50,153 @@ class AEMETEnclosureState(pydantic.BaseModel):
     )
     battery_voltage: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="BattV",
+        validation_alias=pydantic.AliasChoices("BattV", "battery_voltage"),
         description="Battery voltage in volts.",
     )
     logger_panel_temperature: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="PTemp_C",
+        validation_alias=pydantic.AliasChoices("PTemp_C", "logger_panel_temperature"),
         description="Temperature of the logger panel in degrees celsius.",
     )
     auto_mode: Optional[Literal[0, 1]] = pydantic.Field(
         default=None,
-        validation_alias="AUTO_",
+        validation_alias=pydantic.AliasChoices("AUTO_", "auto_mode"),
         description="Enclosure mode. auto if the enclosure is in automatic mode, manual if it is in manual mode.",
     )
     enhanced_security_mode: Optional[Literal[0, 1]] = pydantic.Field(
         default=None,
-        validation_alias="ENHANCED_SECURITY",
+        validation_alias=pydantic.AliasChoices("ENHANCED_SECURITY", "enhanced_security_mode"),
         description="Whether the enhanced security mode is active. 0 if not, 1 if it is active.",
     )
 
     # weather sensor readings
     air_pressure_internal: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="Press_Int",
+        validation_alias=pydantic.AliasChoices("Press_Int", "air_pressure_internal"),
         description="Internal pressure in hPa.",
     )
     air_pressure_external: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="Press_Ext",
+        validation_alias=pydantic.AliasChoices("Press_Ext", "air_pressure_external"),
         description="External pressure in hPa.",
     )
     relative_humidity_internal: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="RH_Int",
+        validation_alias=pydantic.AliasChoices("RH_Int", "relative_humidity_internal"),
         description="Internal relative humidity in percentage.",
     )
     relative_humidity_external: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="RH_Ext",
+        validation_alias=pydantic.AliasChoices("RH_Ext", "relative_humidity_external"),
         description="External relative humidity in percentage.",
     )
     air_temperature_internal: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="AirTC_Int",
+        validation_alias=pydantic.AliasChoices("AirTC_Int", "air_temperature_internal"),
         description="Internal air temperature in degrees celsius.",
     )
     air_temperature_external: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="AirTC_Ext",
+        validation_alias=pydantic.AliasChoices("AirTC_Ext", "air_temperature_external"),
         description="External air temperature in degrees celsius.",
     )
     dew_point_temperature_internal: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="Trocio_Int",
+        validation_alias=pydantic.AliasChoices("Trocio_Int", "dew_point_temperature_internal"),
         description="Internal dew point temperature in degrees celsius.",
     )
     dew_point_temperature_external: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="Trocio_Ext",
+        validation_alias=pydantic.AliasChoices("Trocio_Ext", "dew_point_temperature_external"),
         description="External dew point temperature in degrees celsius.",
     )
     wind_direction: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="Wdir",
+        validation_alias=pydantic.AliasChoices("Wdir", "wind_direction"),
         description="Wind direction in degrees.",
     )
     wind_velocity: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="Wvel",
+        validation_alias=pydantic.AliasChoices("Wvel", "wind_velocity"),
         description="Wind velocity in m/s.",
     )
     rain_sensor_counter_1: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="LLUVIA_1_In",
+        validation_alias=pydantic.AliasChoices("LLUVIA_1_In", "rain_sensor_counter_1"),
         description="Rain sensor counter 1. Negative values indicate no rain, positive values indicate rain. The higher the counter the more/longer it has been raining.",
     )
     rain_sensor_counter_2: Optional[float] = pydantic.Field(
         default=None,
-        validation_alias="LLUVIA_2_In",
+        validation_alias=pydantic.AliasChoices("LLUVIA_2_In", "rain_sensor_counter_2"),
         description="Rain sensor counter 2. Negative values indicate no rain, positive values indicate rain. The higher the counter the more/longer it has been raining.",
     )
 
     # closing reasons
     closed_due_to_rain: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Cerrar_Lluvia",
+        validation_alias=pydantic.AliasChoices("Cerrar_Lluvia", "closed_due_to_rain"),
         description="Whether the cover has been closed due to rain. 0 if not, 1 if it has been closed due to rain.",
     )
     closed_due_to_external_relative_humidity: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Cerrar_RH_Ext",
+        validation_alias=pydantic.AliasChoices(
+            "Cerrar_RH_Ext", "closed_due_to_external_relative_humidity"
+        ),
         description="Whether the cover has been closed due to high external relative humidity. 0 if not, 1 if it has been closed due to high external relative humidity.",
     )
     closed_due_to_external_air_temperature: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Cerrar_AirTC_Ext",
+        validation_alias=pydantic.AliasChoices(
+            "Cerrar_AirTC_Ext", "closed_due_to_external_air_temperature"
+        ),
         description="Whether the cover has been closed due to high external air temperature. 0 if   not, 1 if it has been closed due to high external air temperature.",
     )
     closed_due_to_internal_relative_humidity: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Cerrar_RH_Int",
+        validation_alias=pydantic.AliasChoices(
+            "Cerrar_RH_Int", "closed_due_to_internal_relative_humidity"
+        ),
         description="Whether the cover has been closed due to high internal relative humidity. 0 if not, 1 if it has been closed due to high internal relative humidity.",
     )
     closed_due_to_internal_air_temperature: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Cerrar_AirTC_Int",
+        validation_alias=pydantic.AliasChoices(
+            "Cerrar_AirTC_Int", "closed_due_to_internal_air_temperature"
+        ),
         description="Whether the cover has been closed due to high internal air temperature. 0 if not, 1 if it has been closed due to high internal air temperature.",
     )
     closed_due_to_wind_velocity: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Cerrar_Wvel_Alta",
+        validation_alias=pydantic.AliasChoices("Cerrar_Wvel_Alta", "closed_due_to_wind_velocity"),
         description="Whether the cover has been closed due to high wind velocity. 0 if not, 1 if it has been closed due to high wind velocity.",
     )
     opened_due_to_elevated_internal_humidity: Optional[bool] = pydantic.Field(
         default=None,
-        validation_alias="Abrir_RH_Int_Elevado",
+        validation_alias=pydantic.AliasChoices(
+            "Abrir_RH_Int_Elevado", "opened_due_to_elevated_internal_humidity"
+        ),
         description="Whether the cover has been opened due to elevated internal relative humidity. 0 if not, 1 if it has been opened due to elevated internal relative humidity.",
     )
 
     # cover states
     alert_level: Optional[Literal[0, 1, 2]] = pydantic.Field(
         default=None,
-        validation_alias="ALERTA",
+        validation_alias=pydantic.AliasChoices("ALERTA", "alert_level"),
         description="Alert level. 0 if no alert, 1 if counting towards closing, 2 if should be closed due to alert.",
     )
     averia_fault_code: Optional[int] = pydantic.Field(
         default=None,
-        validation_alias="AVERIA",
+        validation_alias=pydantic.AliasChoices("AVERIA", "averia_fault_code"),
         description="Value of AVERIA.",
     )
     cover_status: Optional[Literal["AF", "A.", "A", "C.", "CF", "C"]] = pydantic.Field(
         default=None,
-        validation_alias="Estado_actual",
+        validation_alias=pydantic.AliasChoices("Estado_actual", "cover_status"),
         description="Current cover status. AF: opening (releasing fechillo), A.: opening, A: open, C.: closing, CF: closing (fechillo), C: closed.",
     )
     motor_position: Optional[int | str] = pydantic.Field(
         default=None,
-        validation_alias="posicion",
+        validation_alias=pydantic.AliasChoices("posicion", "motor_position"),
         description="Current motor position. 0 is fully open, higher values are more closed.",
     )
 
