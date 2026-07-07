f6d23e748a57a9d5a5f231e2c718336f25dfd08a
Moritz Makowski <moritz.makowski@tum.de>
2026-07-07 15:22:09 +0200

    #278 Document exceptions in a central place (3)
    Add more known exceptions

--- packages/core/types/exceptions.py
+++ packages/core/types/exceptions.py
@@ -28,6 +28,24 @@ class KnownException(tum_esm_utils.validators.StricterBaseModel):
 
 class KnownExceptions:
     COVER_DID_NOT_OPEN = KnownException(
-        error_message="CamTracker was started but cover is closed.",
-        long_description="This error might have been caused by the enclosures rain sensor being triggered long enough to cause the cover to be closed but too short for PYRA to read from rain sensor in time. This is usually self-resolving after some time since the cover either opens or the rain sensor is triggered long enough for PYRA to read it. If not resolving, the cover might be stuck/the motor might be broken and needs to be checked.",
+        error_message="Camtracker was started but cover is closed.",
+        long_description=(
+            "This error might have been caused by the enclosures rain sensor being triggered long enough to cause the cover to be closed but too short for PYRA to read from rain sensor in time. This is usually self-resolving after some time since the cover either opens or the rain sensor is triggered long enough for PYRA to read it. If not resolving, the cover might be stuck/the motor might be broken and needs to be checked."
+        ),
+    )
+    STORAGE_ERROR = KnownException(
+        error_message="The system disk is more than 90% full.",
+        long_description=(
+            "The system disk is more than 90% full. This can make the operating system unstable."
+        ),
+    )
+    LOW_ENERGY_ERROR = KnownException(
+        error_message="The system battery level is below 30%.",
+        long_description="The system battery level is below 30%. Check the power supply.",
+    )
+    PYRA_CORE_CRASHED = KnownException(
+        error_message="Pyra Core is not running and has not been shut down properly.",
+        long_description='Pyra core did not shutdown with a graceful shutdown message. Normally this should only happen if the operating system forcefully killed the Pyra core process. This can also happen if the system lost power and booted back up. This error is usually raised by the Pyra UI calling "pyra-cli core is-running" to check whether a proper shutdown happened.',
+    )
+
     )
